bf46a752ee0ea58fae31e33780b1fe142694dae5
Russell Standish <hpcoder@hpcoders.com.au>
2016-08-05 10:07:41 +1000

    Enabling t0035a.sh to check that it runs on Travis ('coz it doesn't on OpenSUSE)

--- test/00/t0035a.sh
+++ test/00/t0035a.sh
@@ -31,7 +31,7 @@ trap "fail" 1 2 3 15
 cp $here/examples/*.mky $here/test/testEq.mky $here/test/test1argBinaryDisplays.mky .
 for i in *.mky; do
   echo $i
-  $here/GUI/minsky $here/test/renderEquations.tcl $i
+  $here/gui-tk/minsky $here/test/renderEquations.tcl $i
   diff $i.gif $here/test/renderedEquations/$i.gif
   if test $? -ne 0; then fail; fi
 done
