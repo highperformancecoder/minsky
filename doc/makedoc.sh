99b1720f7672dd6ac14bbff3027ad2ee812735e1
Russell Standish <hpcoder@hpcoders.com.au>
2016-08-19 13:47:51 +1000

    make a sed invocation work inplace

--- doc/makedoc.sh
+++ doc/makedoc.sh
@@ -11,8 +11,7 @@ if [ $? -ne 0 ]; then exit 1; fi
 # for our in house help file style
 
 cp -f images/* minsky
-sed -e 's/^\(  HREF=[^>]*\)>/\1 target="content">/' <minsky/minsky.html >tmp
-mv tmp minsky/minsky.html
+sed -i -e 's/^\(  HREF=[^>]*\)>/\1 target="content">/' minsky/minsky.html
 
 #check for any broken links
 linkchecker minsky.html
