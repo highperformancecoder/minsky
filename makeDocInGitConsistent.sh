d1d7637b86754ee3e41dae30a803143979c4ba37
Russell Standish <hpcoder@hpcoders.com.au>
2016-08-19 14:27:45 +1000

    Added missing file gui-tk/library/help/minsky.html

--- makeDocInGitConsistent.sh
+++ makeDocInGitConsistent.sh
@@ -2,20 +2,11 @@
 rm -rf doc/minsky
 (cd doc; sh makedoc.sh)
 
-target=GUI/library/help
-aegis -cp GUI/library
-rm -rf $target/*
+target=gui-tk/library/help
+git rm -rf $target/*
 mkdir -p $target/minsky
 find doc/minsky \( -name "*.html" -o -name "*.css" -o -name "*.png" \) -exec cp {} $target/minsky \;
 cp -r -f doc/minsky.html $target
-aegis -list -terse project_files|grep $target|while read nm; do 
-    if [ ! -f $nm ]; then 
-        aegis -cpu $nm 
-        aegis -rm $nm 
-    fi
-done
-find $target -exec aegis -nf {} \;
-aegis -cpu -unch GUI/library
-
-aegis -cp GUI/helpRefDb.tcl
+for i in $target/minsky/*; do git add $i; done
 perl makeRefDb.pl doc/minsky/labels.pl >GUI/helpRefDb.tcl
+git commit -m "Making doc consistent in git"
