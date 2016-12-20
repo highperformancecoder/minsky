dc687508735b7ca00579f77a115b38ce5859b24f
Russell Standish <hpcoder@hpcoders.com.au>
2016-12-20 15:55:10 +1100

    Removed some unused image files. Some other minor cosmetic changes to the manual.

--- doc/makedoc.sh
+++ doc/makedoc.sh
@@ -4,13 +4,13 @@ pdflatex -interaction=batchmode minsky
 if [ $? -ne 0 ]; then exit 1; fi
 
 pdflatex -interaction=batchmode minsky
-latex2html -local_icons -info "" minsky
+latex2html -local_icons -white -notransparent -info "" minsky
 if [ $? -ne 0 ]; then exit 1; fi
 
 # copy images, and mutate table of contents after latex2html suitable
 # for our in house help file style
 
-cp -f images/* minsky
+cp -f images/*.png minsky
 sed -i -e 's/^\(  HREF=[^>]*\)>/\1 target="content">/' minsky/minsky.html
 
 #check for any broken links
