ba628a9df8101316e02deceae3e3ee8160a71c28
hpcoder@hpcoders.com.au <hpcoder@hpcoders.com.au>
2016-12-28 19:30:45 +1100

    Fixed up helpRefdb target in makeDocInGitConsistent

--- makeDocInGitConsistent.sh
+++ makeDocInGitConsistent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf doc/minsky
 (cd doc; sh makedoc.sh)
+git add doc/minsky.pdf
 
 target=gui-tk/library/help
 git rm -rf $target/minsky/*
@@ -8,5 +9,6 @@ mkdir -p $target/minsky
 find doc/minsky \( -name "*.html" -o -name "*.css" -o -name "*.png" \) -exec cp {} $target/minsky \;
 cp -r -f doc/minsky.html $target
 for i in $target/minsky/*; do git add $i; done
-perl makeRefDb.pl doc/minsky/labels.pl >GUI/helpRefDb.tcl
+perl makeRefDb.pl doc/minsky/labels.pl >gui-tk/helpRefDb.tcl
+git add gui-tk/helpRefDb.tcl
 git commit -m "Making doc consistent in git"
