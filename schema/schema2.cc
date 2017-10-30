86f79c09c988449a76f4a3aea361c569ef58c5f3
Russell Standish <hpcoder@hpcoders.com.au>
2017-10-31 10:03:35 +1100

    Added schema2 functionality. Last commit failed to add them.

---
+++ schema/schema2.cc
@@ -0,0 +1,356 @@
+/*
+  @copyright Steve Keen 2017
+  @author Russell Standish
+  This file is part of Minsky.
+
+  Minsky is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Minsky is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Minsky.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "schema2.h"
+#include <ecolab_epilogue.h>
+template <> classdesc::Factory<minsky::Item,string>::Factory() {}
+
+namespace schema2
+{
+  // map of object to ID, that allocates a new ID on objects not seen before
+  struct IdMap: public map<void*,int>
+  {
+    int nextId=0;
+    int operator[](void* o) {
+      auto i=find(o);
+      if (i==end())
+        return emplace(o,nextId++).first->second;
+      else
+        return i->second;
+    }
+    vector<int> operator[](const minsky::ItemPortVector& v) {
+      vector<int> r;
+      for (auto& i: v)
+        r.push_back(operator[](i.get()));
+      return r;
+    }
+    
+    template <class T>
+    bool emplaceIf(vector<Item>& items, minsky::Item* i)
+    {
+      auto j=dynamic_cast<T*>(i);
+      if (j)
+        items.emplace_back(operator[](j), *j, operator[](j->ports));
+      return j;
+    }
+  };
+
+  namespace
+  {
+    inline bool matchesStart(const string& x, const string& y)
+    {
+      size_t n=min(x.length(),y.length());
+      return x.substr(0,n)==y.substr(0,n);
+    }
+  }
+  
+  struct MinskyItemFactory: public classdesc::Factory<minsky::Item,string>
+  {
+    template <class T>
+    void registerClassType() {registerType<T>(typeName<T>());}
+    MinskyItemFactory()
+    {
+      registerClassType<minsky::Item>();
+      registerClassType<minsky::IntOp>();
+      registerClassType<minsky::DataOp>();
+      registerClassType<minsky::GodleyIcon>();
+      registerClassType<minsky::PlotWidget>();
+      registerClassType<minsky::SwitchIcon>();
+    }
+
+    // override here for special handling of Operations and Variables
+    minsky::Item* create(const string& t) const
+    {
+      if (matchesStart(t,"Operation:"))
+        return minsky::OperationBase::create
+          (enum_keys<minsky::OperationType::Type>()
+           (t.substr(t.find(':')+1)));
+      if (matchesStart(t,"VariableBase:"))
+        return minsky::VariableBase::create
+          (enum_keys<minsky::VariableType::Type>()
+           (t.substr(t.find(':')+1)));
+      return classdesc::Factory<minsky::Item,string>::create(t);
+    }
+  };
+  
+  struct Schema1Layout
+  {
+    map<int,schema1::UnionLayout> layout;
+    Schema1Layout(const vector<shared_ptr<schema1::Layout>>& x) {
+      for (auto& i: x)
+        {
+          // serialise to json, then deserialise to a UnionLayout
+          json_pack_t jbuf;
+          i->json_pack(jbuf,"");
+          json_unpack(jbuf,"",layout[i->id]);
+        }
+    }
+    template <class V, class O>
+    void addItem(V& vec, const O& item) {
+      vec.emplace_back(item);
+      if (layout.count(item.id))
+        vec.back().addLayout(layout[item.id]);
+    }
+  };
+  
+  Minsky::Minsky(const schema1::Minsky& m)
+  {
+    // read in layout into a map indexed by id
+    Schema1Layout layout(m.layout);
+    
+    for (auto& i: m.model.wires)
+      layout.addItem(wires,i);
+    for (auto& i: m.model.notes)
+      layout.addItem(items,i);    
+    for (auto& i: m.model.operations)
+      layout.addItem(items,i);    
+
+    // save a mapping of ports to variables for later use by group
+    map<int,pair<int,bool>> portToVar;
+    for (auto& i: m.model.variables)
+      {
+        layout.addItem(items,i);
+        for (size_t j=0; j<i.ports.size(); ++j)
+          portToVar[i.ports[j]]=make_pair(i.id,j>0);
+      }
+    
+    for (auto& i: m.model.plots)
+      layout.addItem(items,i);    
+    for (auto& i: m.model.switches)
+      layout.addItem(items,i);    
+    for (auto& i: m.model.godleys)
+      layout.addItem(items,i);    
+    for (auto& i: m.model.groups)
+      {
+        layout.addItem(groups,i);
+        for (auto p: i.ports)
+          {
+            auto v=portToVar.find(p);
+            if (v!=portToVar.end())
+              if (v->second.second)
+                groups.back().inVariables.push_back(v->second.first);
+              else
+                groups.back().outVariables.push_back(v->second.first);
+          }
+      }
+    
+    rungeKutta=m.model.rungeKutta;
+    zoomFactor=m.zoomFactor;
+  }
+
+  
+  Minsky::Minsky(const minsky::Group& g)
+  {
+    IdMap itemMap;
+
+    g.recursiveDo(&minsky::GroupItems::items,[&](const minsky::Items&,minsky::Items::const_iterator i) {
+        itemMap.emplaceIf<minsky::OperationBase>(items, i->get()) &&
+          itemMap.emplaceIf<minsky::VariableBase>(items, i->get()) &&
+          itemMap.emplaceIf<minsky::GodleyIcon>(items, i->get()) &&
+          itemMap.emplaceIf<minsky::PlotWidget>(items, i->get()) &&
+          itemMap.emplaceIf<minsky::SwitchIcon>(items, i->get()) &&
+          itemMap.emplaceIf<minsky::Item>(items, i->get());
+        return false;
+      });
+    
+    // search for and link up integrals to their variables
+    g.recursiveDo(&minsky::GroupItems::items,[&](const minsky::Items&,minsky::Items::const_iterator i) {
+        if (auto integ=dynamic_cast<minsky::IntOp*>(i->get()))
+          {
+            int id=itemMap[i->get()];
+            for (auto& j: items)
+              if (j.id==id)
+                {
+                  // nb conversion to Item* essential here, as
+                  // conversion of derived class to void* may not be
+                  // equivalent
+                  assert(itemMap.count(static_cast<minsky::Item*>(integ->intVar.get())));
+                  j.intVar.reset(new int(itemMap[static_cast<minsky::Item*>(integ->intVar.get())]));
+                  break;
+                }
+          }
+        return false;
+      });
+        
+    g.recursiveDo(&minsky::GroupItems::wires,
+                  [&](const minsky::Wires&,minsky::Wires::const_iterator i) {
+                    wires.emplace_back(itemMap[i->get()], **i);
+                    assert(itemMap.count((*i)->from().get()) && itemMap.count((*i)->to().get()));
+                    wires.back().from=itemMap[(*i)->from().get()];
+                    wires.back().to=itemMap[(*i)->to().get()];
+                    return false;
+      });
+    
+    g.recursiveDo(&minsky::GroupItems::groups,
+                  [&](const minsky::Groups&,minsky::Groups::const_iterator i) {
+                    groups.emplace_back(itemMap[i->get()], **i);
+                    // insert ports for I/O variables
+                    assert(groups.back().ports.empty());
+                    for (auto& v: (*i)->inVariables)
+                      {
+                        assert(itemMap.count(v->ports[1].get()));
+                        groups.back().inVariables.push_back(itemMap[v->ports[1].get()]);
+                      }
+                    for (auto& v: (*i)->outVariables)
+                      {
+                        assert(itemMap.count(v->ports[0].get()));
+                        groups.back().outVariables.push_back(itemMap[v->ports[0].get()]);
+                      }
+                    return false;
+                  });
+  }
+      
+  Minsky::operator minsky::Minsky() const
+  {
+    minsky::Minsky m;
+    populateGroup(*m.model);
+    m.model->setZoom(zoomFactor);
+    
+    m.stepMin=rungeKutta.stepMin; 
+    m.stepMax=rungeKutta.stepMax; 
+    m.nSteps=rungeKutta.nSteps;   
+    m.epsAbs=rungeKutta.epsAbs;   
+    m.epsRel=rungeKutta.epsRel;   
+    m.order=rungeKutta.order;
+    m.simulationDelay=rungeKutta.simulationDelay;
+    m.implicit=rungeKutta.implicit;
+    return m;
+  }
+
+  void populateNote(minsky::NoteBase& x, const Note& y)
+  {
+    x.detailedText=y.detailedText;
+    x.tooltip=y.tooltip;
+  }
+  
+  void populateItem(minsky::Item& x, const Item& y)
+  {
+    populateNote(x,y);
+    x.m_x=y.x;
+    x.m_y=y.y;
+    x.rotation=y.rotation;
+    if (auto x1=dynamic_cast<minsky::DataOp*>(&x))
+      {
+        if (y.name)
+          x1->description=*y.name;
+        if (y.dataOpData)
+          x1->data=*y.dataOpData;
+      }
+    if (auto x1=dynamic_cast<minsky::VariableBase*>(&x))
+      {
+        if (y.name)
+          x1->name(*y.name);
+        if (y.init)
+          x1->init(*y.init);
+      }
+    if (auto x1=dynamic_cast<minsky::GodleyIcon*>(&x))
+      {
+        std::vector<std::vector<std::string>> data;
+        std::vector<minsky::GodleyAssetClass::AssetClass> assetClasses;
+        if (y.data) data=*y.data;
+        if (y.assetClasses) assetClasses=*y.assetClasses;
+        SchemaHelper::setPrivates(x1->table,data,assetClasses);
+        if (y.height)
+          x1->scaleIconForHeight(*y.height);
+      }
+    if (auto x1=dynamic_cast<minsky::PlotWidget*>(&x))
+      {
+        if (y.width) x1->width=*y.width;
+        if (y.height) x1->height=*y.height;
+        if (y.name) x1->title=*y.name;
+        if (y.logx) x1->logx=*y.logx;
+        if (y.logy) x1->logy=*y.logy;
+        if (y.xlabel) x1->xlabel=*y.xlabel;
+        if (y.ylabel) x1->ylabel=*y.ylabel;
+        if (y.y1label) x1->y1label=*y.y1label;
+        if (y.legend)
+          {
+            x1->legend=true;
+            x1->legendSide=*y.legend;
+          }
+      }
+    if (auto x1=dynamic_cast<minsky::SwitchIcon*>(&x))
+      {
+        auto r=fmod(y.rotation,360);
+        x1->flipped=r>90 && r<270;
+      }
+  }
+
+  void populateWire(minsky::Wire& x, const Wire& y)
+  {
+    populateNote(x,y);
+    x.coords(y.coords);
+  }
+  
+  
+  void Minsky::populateGroup(minsky::Group& g) const {
+    map<int, minsky::ItemPtr> itemMap;
+    map<int, shared_ptr<minsky::Port>> portMap;
+    map<int, minsky::GroupPtr> groupMap;
+    MinskyItemFactory factory;
+    
+    for (auto& i: items)
+      {
+        auto newItem=itemMap[i.id]=g.addItem(factory.create(i.type));
+        for (size_t j=0; j<min(newItem->ports.size(), i.ports.size()); ++j)
+          portMap[i.ports[j]]=newItem->ports[j];
+        populateItem(*newItem,i);
+      }
+
+    for (auto& w: wires)
+      if (portMap.count(w.to) && portMap.count(w.from))
+        populateWire
+          (*g.addWire(new minsky::Wire(portMap[w.from],portMap[w.to],w.coords)),
+           w);
+
+    for (auto& i: groups)
+      itemMap[i.id]=g.addGroup(new minsky::Group);
+
+    // second loop over groups, because groups can contain other groups
+    for (auto& i: groups)
+      {
+        assert(itemMap.count(i.id));
+        auto newG=dynamic_cast<minsky::Group*>(itemMap[i.id].get());
+        if (newG)
+          {
+            for (auto j: i.items)
+              {
+                auto it=itemMap.find(j);
+                if (it!=itemMap.end())
+                  newG->addItem(it->second);
+              }
+            for (auto j: i.inVariables)
+              {
+                auto it=itemMap.find(j);
+                if (it!=itemMap.end())
+                  if (auto v=dynamic_pointer_cast<minsky::VariableBase>(it->second))
+                    newG->inVariables.push_back(v);
+              }
+            for (auto j: i.outVariables)
+              {
+                auto it=itemMap.find(j);
+                if (it!=itemMap.end())
+                  if (auto v=dynamic_pointer_cast<minsky::VariableBase>(it->second))
+                    newG->outVariables.push_back(v);
+              }
+          }
+      }
+  }
+}
+
+
---
+++ schema/schema2.h
@@ -0,0 +1,255 @@
+/*
+  @copyright Steve Keen 2017
+  @author Russell Standish
+  This file is part of Minsky.
+
+  Minsky is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Minsky is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Minsky.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+   @file schema 2 is a defined and published Minsky schema. It is
+expected to grow over time, deprecated attributes are also allowed,
+but any renamed attributes require bumping the schema number.  
+*/
+#ifndef SCHEMA_2_H
+#define SCHEMA_2_H
+
+#include "model/minsky.h"
+#include "schema/schema1.h"
+#include "schemaHelper.h"
+#include "classdesc.h"
+#include "polyXMLBase.h"
+#include "polyJsonBase.h"
+
+#include <xsd_generate_base.h>
+#include <vector>
+#include <string>
+
+namespace schema2
+{
+
+  using minsky::SchemaHelper;
+  using namespace std;
+  using namespace classdesc;
+  using classdesc::shared_ptr;
+
+
+  // see discussion in Stroustrup 4th ed 28.4.4
+  template <class T> struct has_empty
+  {
+    template <class X>
+    static auto check(X x)->decltype(x.empty());
+    static std::false_type check(...);
+    
+    static constexpr const bool value = std::is_integral<decltype(check(std::declval<T>()))>::value;
+  };
+   
+  /// convenience class to omit writing XML records when data absent or empty
+  template <class T>
+  struct Optional: std::shared_ptr<T>
+  {
+    Optional() {}
+    Optional(const T& x) {assign(x);}
+    template <class U>
+    typename classdesc::enable_if<has_empty<U>,void>::T
+    assign(const U& x, classdesc::dummy<0> d=0) {
+      if (!x.empty()) this->reset(new U(x));
+    }
+    template <class U>
+    typename classdesc::enable_if<Not<has_empty<U>>,void>::T
+    assign(const U& x, classdesc::dummy<1> d=0) {this->reset(new U(x));}
+  };
+
+  struct Note
+  {
+    std::string detailedText, tooltip;
+    Note() {}
+    template <class T>
+    Note(const T& x): detailedText(x.detailedText), tooltip(x.tooltip) {}
+  };
+
+  // attribute common to all items
+  struct ItemBase: public Note
+  {
+    int id=-1;
+    std::string type;
+    float x=0, y=0; ///< position in canvas, or within group
+    float zoomFactor=1;
+    double rotation=0; ///< rotation of icon, in degrees
+    std::vector<int> ports;
+    ItemBase() {}
+    ItemBase(int id, const minsky::Item& it, const std::vector<int>& ports): 
+      Note(it), id(id), type(it.classType()),
+      x(it.m_x), y(it.m_y), zoomFactor(it.zoomFactor), rotation(it.rotation),
+      ports(ports) {}
+    ItemBase(const schema1::Item& it, const std::string& type="Item"):
+      Note(it), id(it.id), type(type) {}
+  };
+  
+  struct Item: public ItemBase
+  {
+    Optional<float> width, height;
+    Optional<std::string> name; //name, description or title
+    Optional<std::string> init;
+    std::shared_ptr<int> intVar;
+    Optional<std::map<double,double>> dataOpData;
+    // Godley Icon specific fields
+    Optional<std::vector<std::vector<std::string>>> data;
+    Optional<std::vector<minsky::GodleyAssetClass::AssetClass>> assetClasses;
+    // Plot specific fields
+    Optional<bool> logx, logy;
+    Optional<std::string> xlabel, ylabel, y1label;
+    std::shared_ptr<ecolab::Plot::Side> legend;
+
+    Item() {}
+    Item(int id, const minsky::Item& it, const std::vector<int>& ports): ItemBase(id,it,ports) {}
+    // minsky object importers
+    Item(const schema1::Item& it): ItemBase(it) {}
+    Item(int id, const minsky::VariableBase& v, const std::vector<int>& ports):
+      ItemBase(id,static_cast<const minsky::Item&>(v),ports),
+      name(v.rawName()), init(v.init()) {}
+    Item(int id, const minsky::GodleyIcon& g, const std::vector<int>& ports):
+      ItemBase(id,static_cast<const minsky::Item&>(g),ports),
+      width(g.width()), height(g.height()), name(g.table.title), data(g.table.getData()),
+      assetClasses(g.table._assetClass()) {}
+    Item(int id, const minsky::PlotWidget& p, const std::vector<int>& ports):
+      ItemBase(id,static_cast<const minsky::Item&>(p),ports),
+      width(p.width), height(p.height), name(p.title), logx(p.logx), logy(p.logy),
+      xlabel(p.xlabel), ylabel(p.ylabel), y1label(p.y1label),
+      legend(p.legend? new ecolab::Plot::Side(p.legendSide): nullptr) {}
+    Item(int id, const minsky::SwitchIcon& s, const std::vector<int>& ports):
+      ItemBase(id, static_cast<const minsky::Item&>(s),ports) 
+    {if (s.flipped) rotation=180;}
+
+    // schema1 importers
+    Item(const schema1::Operation& it):
+      ItemBase(it, "Operation:"+minsky::OperationType::typeName(it.type)),
+      name(it.name), intVar(it.intVar>-1? new int(it.intVar): nullptr),
+      dataOpData(it.data) {
+      ports=it.ports;
+      // rewrite deprecated constants as variables
+      if (it.type==minsky::OperationType::constant)
+        {
+          type="Variable:constant";
+          init.reset(new std::string(std::to_string(it.value)));
+        }
+    }
+    Item(const schema1::Variable& it):
+      ItemBase(it, "Variable:"+minsky::VariableType::typeName(it.type)),
+      init(it.init), name(it.name) {ports=it.ports;}
+    Item(const schema1::Godley& it):
+      ItemBase(it, "GodleyIcon"),
+      name(it.name), data(it.data), assetClasses(it.assetClasses) {ports=it.ports;}
+    Item(const schema1::Plot& it):
+      ItemBase(it, "PlotWidget"),
+      name(it.title), logx(it.logx), logy(it.logy), xlabel(it.xlabel), ylabel(it.ylabel), y1label(it.y1label),
+      legend(it.legend) {ports=it.ports;}
+    Item(const schema1::Group& it): ItemBase(it,"Group") {} 
+
+    void addLayout(const schema1::UnionLayout& layout) {
+      x=layout.x;
+      y=layout.y;
+      rotation=layout.rotation;
+      width.reset(new float(layout.width));
+      height.reset(new float(layout.height));
+    }
+  };
+
+
+  struct Wire: public Note
+  {
+    int id=-1;
+    int from=-1, to=-1;
+    vector<float> coords;
+    Wire() {}
+    Wire(int id, const minsky::Wire& w): Note(w), id(id) {}
+    Wire(const schema1::Wire& w): Note(w), id(w.id), from(w.from), to(w.to) {}
+    void addLayout(const schema1::UnionLayout& layout) {coords=layout.coords;}
+  };
+
+  struct Group: public Item
+  {
+    vector<int> items;
+    vector<int> inVariables, outVariables;
+    Group() {}
+    Group(int id, const minsky::Group& g): Item(id,g,std::vector<int>()) {}
+
+    /// note this assumes that ids have been uniquified prior to this call
+    Group(const schema1::Group& g):
+      Item(g), items(g.items) {}
+  };
+
+
+  struct RungeKutta
+  {
+    double stepMin{0}, stepMax{0.01};
+    int nSteps{1};
+    double epsRel{1e-2}, epsAbs{1e-3};
+    int order{4};
+    bool implicit{false};
+    int simulationDelay{0};
+    RungeKutta() {}
+    RungeKutta(const minsky::Minsky& m):
+      stepMin(m.stepMin), stepMax(m.stepMax), nSteps(m.nSteps),
+      epsRel(m.epsRel), epsAbs(m.epsAbs), order(m.order), 
+      implicit(m.implicit), simulationDelay(m.simulationDelay) {}
+    RungeKutta(const schema1::RungeKutta& m):
+      stepMin(m.stepMin), stepMax(m.stepMax), nSteps(m.nSteps),
+      epsRel(m.epsRel), epsAbs(m.epsAbs), order(m.order), 
+      implicit(m.implicit), simulationDelay(m.simulationDelay) {}
+    
+  };
+
+  struct Minsky
+  {
+    static const int version=2;
+    int schemaVersion=Minsky::version;
+    vector<Wire> wires;
+    vector<Item> items;
+    vector<Group> groups;
+    Group model;
+    RungeKutta rungeKutta;
+    double zoomFactor=1;
+
+    /// checks that all items are uniquely identified.
+    bool validate() const;
+    Minsky(): schemaVersion(-1) {} // schemaVersion defined on read in
+    Minsky(const minsky::Group& g);
+    Minsky(const minsky::Minsky& m): Minsky(*m.model) {
+      rungeKutta=RungeKutta(m);
+      zoomFactor=m.model->zoomFactor;
+      assert(validate());
+    }
+
+    Minsky(const schema1::Minsky& m);
+    
+    /// create a Minsky model from this
+    operator minsky::Minsky() const;
+    /// populate a group object from this. This mutates the ids in a
+    /// consistent way into the free id space of the global minsky
+    /// object
+    void populateGroup(minsky::Group& g) const;
+    /// move locations such that minx, miny lies at (0,0) on canvas
+    void relocateCanvas();
+
+  };
+
+
+}
+
+
+#include "schema2.cd"
+#include "schema2.xcd"
+
+#endif
