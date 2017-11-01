0c6cabef844961dc424a4abb6fd66f398f5f6a74
Russell Standish <hpcoder@hpcoders.com.au>
2017-11-01 17:03:41 +1100

    Fixed all regression tests for importing via schema 2

--- test/checkOverrides.tcl
+++ test/checkOverrides.tcl
@@ -3,12 +3,17 @@ minsky.load $argv(2)
 set numItems [minsky.model.items.size]
 for {set i 0} {$i<$numItems} {incr i} {
     minsky.model.items.@elem $i
-    set origClassType [minsky.model.items(0).classType]
-    if [minsky.getItemAt [minsky.model.items(0).x] [minsky.model.items(0).y]] {
+    if [minsky.getItemAt [minsky.model.items($i).x] [minsky.model.items($i).y]] {
+        set origClassType [minsky.canvas.item.classType]
         minsky.canvas.copyItem
         minsky.canvas.mouseUp -500 -500
         assert {[minsky.getItemAt -500 -500]}
-        assert "\"$origClassType\"==\[minsky.canvas.item.classType\]" "$argv(2)"
+        if {$origClassType=="IntOp"} {
+            assert {"Variable:integral"==[minsky.canvas.item.classType]} "$argv(2)"
+        } else {
+            assert "\"$origClassType\"==\[minsky.canvas.item.classType\]" "$argv(2)"
+        }
+        minsky.canvas.item.moveTo -1000 -1000
     }
 }
 tcl_exit   
