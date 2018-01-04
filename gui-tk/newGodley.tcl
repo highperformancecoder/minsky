9c8d39fbd485ec211f95b8dbdae25727f99caa8f
Russell Standish <hpcoder@hpcoders.com.au>
2018-01-04 16:56:32 +1100

    Enable godley to expand when window resized.

--- gui-tk/newGodley.tcl
+++ gui-tk/newGodley.tcl
@@ -3,7 +3,7 @@ proc newOpenGodley {id} {
         image create cairoSurface $id -surface $id
         toplevel .$id
         label .$id.table -image $id -width 500 -height 100
-        pack .$id.table
+        pack .$id.table -fill both -expand 1
         bind .$id.table <Configure> "$id.requestRedraw"
         bind .$id.table <Destroy> "$id.delete"
     }
