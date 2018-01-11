86acc01f4cdc143dfe073a0739d9f5720f584c12
Russell Standish <hpcoder@hpcoders.com.au>
2018-01-11 14:29:46 +1100

    Split Travis script into two that can run independently

---
+++ travisRun.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+if [ $TEST_SUITE=sure]; then 
+    git clone https://github.com/highperformancecoder/ecolab.git
+    pushd ecolab
+    git submodule init
+    git submodule update
+    make install
+    popd
+    make DEBUG=1
+    Xvfb :0 &>/dev/null &
+    export DISPLAY=:0
+    export MINSKY_TEST_DATABASE_PARAMS=$HOME/.testDatabase
+    echo "testEq.mky" >$MINSKY_TEST_DATABASE_PARAMS
+    psql -U postgres <server/minskyPG.sql
+    echo "postgresql://dbname=postgres user=postgres password=">>$MINSKY_TEST_DATABASE_PARAMS
+    export TRAVIS=1
+# This step generates reference image files on the CI server 
+#  - sh createRenderedEquations
+    make AEGIS=1 sure
+else if [ $TEST_SUITE=docs]; then
+    make doc
+fi
