a1674eb71e56c6db2f5852222e5fa105e8b8258c
Russell Standish <hpcoder@hpcoders.com.au>
2018-06-18 18:04:58 +1000

    Implemented CSV data export from plots. For #840.

--- gui-tk/wiring.tcl
+++ gui-tk/wiring.tcl
@@ -490,6 +490,7 @@ proc contextMenu {x y X Y} {
             .wiring.context add command -label "Make Group Plot" -command "$item.makeDisplayPlot"
             .wiring.context add command -label "Resize" -command "canvas.lassoMode itemResize"
             .wiring.context add command -label "Options" -command "doPlotOptions $item"
+            .wiring.context add command -label "Export as CSV" -command exportItemAsCSV
         }
         "GodleyIcon" {
             .wiring.context add command -label "Open Godley Table" -command "openGodley [minsky.openGodley]"
