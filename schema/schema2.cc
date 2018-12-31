7f430648e88caf4170a0be83eb784a795841718e
Russell Standish <hpcoder@hpcoders.com.au>
2018-12-31 12:36:51 +1100

    compressio0n of cdata records for tensor parameters reduce chatter on make sure

--- schema/schema2.cc
+++ schema/schema2.cc
@@ -21,6 +21,7 @@
 #include <ecolab_epilogue.h>
 
 #include "a85.h"
+#include <zlib.h>
 
 namespace classdesc {template <> Factory<minsky::Item,string>::Factory() {}}
 
@@ -203,6 +204,75 @@ namespace schema2
       }
   }
 
+  namespace
+  {
+    // nice RAII wrappers around zlib's data structures
+    struct ZStream: public z_stream
+    {
+      ZStream(Bytef* input, size_t inputSize, Bytef* output, size_t outputSize)
+      {
+        next_in=input;
+        avail_in=inputSize;
+        next_out=output;
+        avail_out=outputSize;
+        zfree=Z_NULL;
+        zalloc=Z_NULL;
+      }
+      void throwError() const {
+        throw runtime_error(string("compression failure: ")+(msg? msg:""));
+      }
+    };
+    
+    struct DeflateZStream: public ZStream
+    {
+      template <class I, class O>
+      DeflateZStream(const I& input, O& output):
+        ZStream((Bytef*)input.data(), input.size(),
+                (Bytef*)output.data(), output.size())
+      {
+        if (deflateInit(this,9)!=Z_OK) throwError();
+      }
+      ~DeflateZStream() {deflateEnd(this);}
+      void deflate() {
+        if (::deflate(this,Z_FINISH)!=Z_STREAM_END) throwError();
+      }
+     };
+    
+    struct InflateZStream: public ZStream
+    {
+      classdesc::pack_t output{256};
+      Bytef* inputData;
+      size_t inputSize;
+      
+      template <class I>
+      InflateZStream(const I& input):
+        ZStream((Bytef*)input.data(), input.size(), 0,0),
+        inputData((Bytef*)input.data()),inputSize(input.size())
+      {
+        next_out=(Bytef*)output.data();
+        avail_out=output.size();
+        if (inflateInit(this)!=Z_OK) throwError();
+      }
+      ~InflateZStream() {inflateEnd(this);}
+
+      void inflate() {
+        int err;
+        while ((err=::inflate(this,Z_SYNC_FLUSH))==Z_OK)
+          {
+            // try doubling size
+            output.resize(2*output.size());
+            next_out=(Bytef*)(output.data())+total_out;
+            avail_out=output.size()-total_out;
+            next_in=inputData+total_in;
+            avail_in=inputSize-total_in;
+          }
+        if (err!=Z_STREAM_END) throwError();
+      }
+      void throwError() {
+        throw runtime_error(string("compression failure: ")+(msg? msg:""));
+      }
+    };
+ }
   
   void Item::packTensorInit(const minsky::VariableBase& v)
   {
@@ -211,19 +281,24 @@ namespace schema2
         {
           pack_t buf;
           buf<<val->tensorInit<<val->xVector;
-          vector<char> cbuf(a85::size_for_a85(buf.size(),false));
-          a85::to_a85(reinterpret_cast<const unsigned char*>(buf.data()),
-                      buf.size(), &cbuf[0], false);
+          cout << buf.size() << endl;
+          
+          vector<unsigned char> zbuf(buf.size());
+          DeflateZStream zs(buf, zbuf);
+          zs.deflate();
+          
+          vector<char> cbuf(a85::size_for_a85(zs.total_out,false));
+          a85::to_a85(&zbuf[0],zs.total_out, &cbuf[0], false);
           tensorData.reset(new CDATA(cbuf.begin(),cbuf.end()));
-#ifndef NDEBUG
-          assert(buf.size()==a85::size_for_bin(cbuf.size()));
-          assert(cbuf.size()==tensorData->size());
-          assert(memcmp(&(*tensorData)[0],&cbuf[0],cbuf.size())==0);
-          pack_t buf1(buf.size());
-          a85::from_a85(tensorData->c_str(), tensorData->size(),
-                        reinterpret_cast<unsigned char*>(buf1.data()));
-          assert(memcmp(buf.data(),buf1.data(),buf.size())==0);
-#endif
+//#ifndef NDEBUG
+//          assert(buf.size()==a85::size_for_bin(cbuf.size()));
+//          assert(cbuf.size()==tensorData->size());
+//          assert(memcmp(&(*tensorData)[0],&cbuf[0],cbuf.size())==0);
+//          pack_t buf1(buf.size());
+//          a85::from_a85(tensorData->c_str(), tensorData->size(),
+//                        reinterpret_cast<unsigned char*>(buf1.data()));
+//          assert(memcmp(buf.data(),buf1.data(),buf.size())==0);
+//#endif
         }
   }
 
@@ -430,11 +505,16 @@ namespace schema2
               string trimmed; //trim whitespace
               for (auto c: *y.tensorData)
                 if (!isspace(c)) trimmed+=c;
-              pack_t buf(a85::size_for_bin(trimmed.size()));
-              a85::from_a85(trimmed.data(), trimmed.size(),
-                            reinterpret_cast<unsigned char*>(buf.data()));
+
+              vector<unsigned char> zbuf(a85::size_for_bin(trimmed.size()));
+              a85::from_a85(trimmed.data(), trimmed.size(),zbuf.data());
+              
+              InflateZStream zs(zbuf);
+              zs.inflate();
+              cout << zs.total_out << endl;
+              
               vector<minsky::XVector> xv;
-              buf>>val->tensorInit>>xv;
+              zs.output>>val->tensorInit>>xv;
               val->setXVector(xv);
             }
       }
