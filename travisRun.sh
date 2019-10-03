dfc87c02905031edd310f078f8091c567745c556
Russell Standish <hpcoder@hpcoders.com.au>
2019-10-03 16:46:30 +1000

    Add back in an explicit EcoLab build - Travis needs it for some bizarre reason.

--- travisRun.sh
+++ travisRun.sh
@@ -2,6 +2,10 @@
 set -e
 git submodule update --init --recursive
 
+pushd ecolab
+make -j2 all-without-models
+popd
+
 make -j2 DEBUG=1
 export TRAVIS=1
 # This step generates reference image files on the CI server 
