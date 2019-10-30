00804cb302bd3a4b523fb2fdcee30c4737eadbca
Russell Standish <hpcoder@hpcoders.com.au>
2019-10-30 18:51:52 +1100

    Add inline to multiple refed function

--- engine/variableValue.h
+++ engine/variableValue.h
@@ -276,7 +276,7 @@ namespace classdesc
 {
   // specialise for VariableValues to give it an associative container flavour.
   // defining is_associative_container type attribute doesn't work
-  template <>
+  template <> inline
   void RESTProcess(RESTProcess_t& t, const string& d, minsky::VariableValues& a)
   {t.add(d, new RESTProcessAssociativeContainer<minsky::VariableValues>(a));}
 }
