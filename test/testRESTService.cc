d00b94e7788cf59e28c422bbe957b121642cdb2d
Russell Standish <hpcoder@hpcoders.com.au>
2019-11-19 08:47:38 +1100

    Added some more RESTService tests

--- test/testRESTService.cc
+++ test/testRESTService.cc
@@ -36,13 +36,20 @@ SUITE(RESTService)
   
   TEST_FIXTURE(TestFixture, containers)
     {
+      json_pack_t nullj(json_spirit::mValue::null);
       process("/minsky/load","GoodwinLinear02.mky");
-      auto jmodel=process("/minsky/model",{});
+      auto jmodel=process("/minsky/model",nullj);
       CHECK_EQUAL(json(*minsky.model), write(jmodel));
-      auto items=process("/minsky/model/items",{});
+      auto items=process("/minsky/model/items",nullj);
       CHECK_EQUAL(minsky.model->items.size(), items.get_array().size());
       CHECK_EQUAL(minsky.model->items.size(), process("/minsky/model/items/@size",{}).get_int());
-      auto item0=process("/minsky/model/items/@elem/0",{});
+      auto item0=process("/minsky/model/items/@elem/0",nullj);
       CHECK_EQUAL(json(minsky.model->items[0]), write(item0));
+
+      
+      CHECK_EQUAL(minsky.variableValues.size(), process("/minsky/variableValues/@size",nullj).get_int());
+      CHECK_EQUAL(minsky.variableValues.size(), process("/minsky/variableValues/",nullj).get_array().size());
+      CHECK_EQUAL(json(minsky.variableValues[":K"]), write(process("/minsky/variableValues/@elem/:K/second",nullj)));
+      CHECK_EQUAL("K", process("/minsky/variableValues/@elem/:K/second/name",nullj).get_str());
     }
 }
