0d4db61c8e0d6e6a73048df9e6f99ab05dcb0ab6
Russell Standish <hpcoder@hpcoders.com.au>
2019-12-01 12:12:29 +1100

    Enable error message reporting on t0036a.sh, which is mysteriously failing on Travis.

--- test/runtests.sh
+++ test/runtests.sh
@@ -4,15 +4,15 @@ t=0
 for i in test/00/*.sh; do
     echo -n "$i: "
     # rendering is too unstable, so disable on the Travis platform
-    if [ "$TRAVIS" = 1 -a $i = test/00/t0035a.sh ]; then continue; fi
-    if [ "$TRAVIS" = 1 -a $i = test/00/allItemsRenderCheck.sh ]; then continue; fi
-    if [ "$TRAVIS" = 1 -a $i = test/00/godleyTableWindow.sh ]; then continue; fi
+    if [ "$TRAVIS" = 1 -a $i = test/00/t0035a.sh ]; then echo "disabled"; continue; fi
+    if [ "$TRAVIS" = 1 -a $i = test/00/allItemsRenderCheck.sh ]; then echo "disabled"; continue; fi
+    if [ "$TRAVIS" = 1 -a $i = test/00/godleyTableWindow.sh ]; then echo "disabled"; continue; fi
     # this test is hanging on Travis (well any Ubuntu 14.04 environment) for some mysterious reason
-    if [ "$TRAVIS" = 1 -a $i = test/00/canvasContext.sh ]; then continue; fi
+    if [ "$TRAVIS" = 1 -a $i = test/00/canvasContext.sh ]; then echo "disabled"; continue; fi
     # schema validation - disable test for now
-    if [ "$TRAVIS" = 1 -a $i = test/00/t0007a.sh ]; then continue; fi
+    if [ "$TRAVIS" = 1 -a $i = test/00/t0007a.sh ]; then echo "disabled"; continue; fi
     # Why TF is Travis failing this test????
-    if [ "$TRAVIS" = 1 -a $i = test/00/t0008a.sh ]; then continue; fi
+    if [ "$TRAVIS" = 1 -a $i = test/00/t0008a.sh ]; then echo "disabled"; continue; fi
     # disable RESTProcess for now
     if [ $i = test/00/RESTService.sh ]; then echo disabled; continue; fi
     # we need to expose output generated here to prevent a build timeout
@@ -21,6 +21,8 @@ for i in test/00/*.sh; do
     # t0029a.sh tests that saving over a readonly file is prevented. Test doesn't work as root
     elif [ `whoami` = 'root' -a $i = test/00/t0029a.sh ]; then
         su minsky $i &> /dev/null
+    elif [ $i = test/00/t0036a.sh ]; then
+        sh $i 
     else
         sh $i &> /dev/null
     fi
