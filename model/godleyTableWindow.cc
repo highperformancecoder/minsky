884116e097d9afe822a3b2e3473d4386b0048567
wdednam <wdednam@gmail.com>
2019-12-26 20:55:39 +0100

    Partial fix for ticket 1053. a) A-L-E=0 rule no longer applies to column headings. b) Moving entire columns limited to data columns only.

--- model/godleyTableWindow.cc
+++ model/godleyTableWindow.cc
@@ -486,7 +486,7 @@ namespace minsky
       return;  
     else if (selectedRow==0)
       {
-        if (c>0 && selectedCol!=0 && c!=selectedCol)      // Disallow moving flow labels column. For ticket 1064/1066
+        if (c>0 && size_t(c)<godleyIcon->table.cols() && selectedCol>0 && size_t(selectedCol)<godleyIcon->table.cols() && c!=selectedCol)  // Disallow moving flow labels column. For ticket 1064/1066
           godleyIcon->table.moveCol(selectedCol,c-selectedCol);
       }
     else if (r>0 && selectedCol==0)
