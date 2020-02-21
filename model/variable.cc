457056c3a4eebcf94b1d064af89a1b9744d3a5a6
wdednam <wdednam@gmail.com>
2020-02-21 11:41:07 +1100

    Display NaNs and divides by zeroes as ??? or +/- infs

--- model/variable.cc
+++ model/variable.cc
@@ -472,8 +472,19 @@ void VariableBase::draw(cairo_t *cairo) const
       auto val=engExp();
   
       Pango pangoVal(cairo);
-      pangoVal.setFontSize(6*z);
-      pangoVal.setMarkup(mantissa(val));
+      if (isfinite(value())) {
+		   pangoVal.setFontSize(6*z);
+		   pangoVal.setMarkup(mantissa(val));
+	   }
+      else if (!isfinite(value())) { // Display divide by zero as infinity. For ticket 1155
+		  pangoVal.setFontSize(12*z);
+		  if (!signbit(value())) pangoVal.setMarkup("-∞");
+          else pangoVal.setMarkup("∞");
+	  }
+	  else {  // Display any other NaN case as ???. For ticket 1155
+		  pangoVal.setFontSize(6*z);
+		  pangoVal.setMarkup("???");
+	  }
       pangoVal.angle=angle+(notflipped? 0: M_PI);
 
       cairo_move_to(cairo,r.x(w-pangoVal.width()-2,-h-hoffs+2),
