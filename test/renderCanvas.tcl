0139fb5b7dce9468ce57f63999bdedf6671ca3da
Russell Standish <hpcoder@hpcoders.com.au>
2020-02-25 15:59:28 +1100

    Update reference iages and add srand to make the oldSchema render canvas deterministic.

--- test/renderCanvas.tcl
+++ test/renderCanvas.tcl
@@ -4,6 +4,7 @@
 minsky.defaultFont Sans
 minsky.setGodleyIconResource $minskyHome/icons/bank.svg
 minsky.setGroupIconResource $minskyHome/icons/group.svg
+minsky.srand 10
 minsky.load $argv(2)
 minsky.renderCanvasToSVG $argv(3)
 tcl_exit
