50932b00a79cc2e5b1c2bb2493d457209f31b2fb
Russell Standish <hpcoder@hpcoders.com.au>
2020-02-25 15:26:34 +1100

    Rename some tests Rendering tests in oldSchema

---
+++ test/renderCanvas.tcl
@@ -0,0 +1,10 @@
+#!minsky
+
+# a crummy little utility for dumping the rendered canvases as a svg files
+minsky.defaultFont Sans
+minsky.setGodleyIconResource $minskyHome/icons/bank.svg
+minsky.setGroupIconResource $minskyHome/icons/group.svg
+minsky.load $argv(2)
+minsky.renderCanvasToSVG $argv(3)
+tcl_exit
+
