3643da2fcbd8e14ecd15c7269902dabde3d4db83
wdednam <wdednam@gmail.com>
2020-03-09 20:18:13 +0100

    Import variable dropdown buttons no longer activate column move warning message in Godley tables

--- model/godleyTableWindow.cc
+++ model/godleyTableWindow.cc
@@ -108,7 +108,7 @@ namespace minsky
   void ButtonWidget<ButtonWidgetEnums::col>::invoke(double x)
   {
     int button=x/buttonSpacing;
-    if (!godleyIcon.table.multipleEquities) {
+    if (!godleyIcon.table.multipleEquities) {  // no column widgets on equity column in single equity column mode
       if (pos!=last)
         switch (button)
          {
@@ -201,7 +201,14 @@ namespace minsky
           {
             CairoSave cs(surface->cairo());
             cairo_move_to(surface->cairo(), x, columnButtonsOffset);
-            colWidgets[col].draw(surface->cairo());
+            // Prevent the existence of more than one equity column in single equity column mode
+            if (godleyIcon->table._assetClass(col)==GodleyAssetClass::equity && col!=godleyIcon->table.cols()-1 && !godleyIcon->table.multipleEquities) {
+               godleyIcon->table.deleteCol(col);
+               colWidgets[col].draw(surface->cairo());
+		   }
+            else {
+			   colWidgets[col].draw(surface->cairo());   
+		   }
           }
       
         if (col>1)
@@ -312,7 +319,7 @@ namespace minsky
     cairo_set_line_width(surface->cairo(),0.5);
     cairo_stroke(surface->cairo());
 
-    cairo_move_to(surface->cairo(),x-pulldownHot,topTableOffset);
+    //cairo_move_to(surface->cairo(),x-pulldownHot,topTableOffset);
 
     // now row sum column
     x+=3;
@@ -593,7 +600,7 @@ namespace minsky
         selectedRow<int(table.rows()) && (selectedCol!=0 || selectedRow!=1)) // Cell (1,0) is off-limits. For ticket 1064
           {			  	  
             auto& str=table.cell(selectedRow,selectedCol);
-            if (utf8.length() && (keySym<0x7f || (0xffaa <= keySym && keySym <= 0xffbf)))  // Enable numeric keypad key presses. For ticket 1136
+            if (utf8.length() && (keySym<0x7f || (0xffb0 < keySym && keySym < 0xffbe)))  // Enable numeric keypad key presses. For ticket 1136
               // all printing and control characters have keysym
               // <0x80. But some keys (eg tab, backspace and escape
               // are mapped to control characters
@@ -639,10 +646,13 @@ namespace minsky
                   selectedRow=selectedCol=-1;
                   break;
                 case 0xff0d: //return
-                case 0xff8d: //enter added for ticket 1122                            
                   update();                                    
                   selectedRow=selectedCol=-1;                       
                   break;
+                case 0xff8d: //enter added for ticket 1122                            
+                  update();
+                  selectedRow=selectedCol=-1;                  
+                  break;     
                 case 0xff51: //left arrow
                   if (insertIdx>0) insertIdx--;
                   else navigateLeft();
@@ -900,7 +910,7 @@ namespace {
 		    auto swapVar=godleyIcon->table.cell(0,selectedCol);
 		    auto oldAssetClass=godleyIcon->table._assetClass(selectedCol);
 		    auto targetAssetClass=godleyIcon->table._assetClass(c);
-		    if (!swapVar.empty()) {
+		    if (!swapVar.empty() && !(colLeftMargin[c+1]-x < pulldownHot)) { // ImportVar dropdown button should not trigger this condition. For ticket 1162
 		      if (targetAssetClass!=oldAssetClass && targetAssetClass!=GodleyAssetClass::equity && targetAssetClass!=GodleyAssetClass::noAssetClass)
 		         tmpStr=constructMessage(targetAssetClass,oldAssetClass,swapVar);
 		      else if ((targetAssetClass==GodleyAssetClass::equity || targetAssetClass==GodleyAssetClass::noAssetClass) || oldAssetClass==GodleyAssetClass::noAssetClass)
@@ -1116,7 +1126,7 @@ namespace {
   {	    
     CairoSave cs(cairo);
     int idx=0;
-    if (!godleyIcon.table.multipleEquities || rowCol==row) {
+    if (!godleyIcon.table.multipleEquities || rowCol==row) {  // no column widgets on equity column in single equity column mode
       if (rowCol == row || (rowCol == col && pos!=last)) 
         drawButton(cairo,"+",0,1,0,idx++);
       if ((rowCol == row && pos!=first && pos!=firstAndLast) || (rowCol == col && pos!=last)) 	// no delete button for first row containing initial conditions. For ticket 1064
