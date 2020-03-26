aa7fa6e6044338b2dc9e62cd57c3df551ca22af8
wdednam <wdednam@gmail.com>
2020-03-26 21:00:42 +0100

    first pass at downloading CSV files from the web and loading them via CSVDialog.cc

--- gui-tk/csvImport.tcl
+++ gui-tk/csvImport.tcl
@@ -1,4 +1,5 @@
 # assume canvas.minsky.item is the variable
+package require http
 CSVDialog csvDialog
 proc CSVImportDialog {} {
     global workDir csvParms
@@ -114,6 +115,129 @@ proc CSVImportDialog {} {
         raise .wiring.csvImport
         csvDialog.requestRedraw
     }
+
+}
+
+proc CSVWebImportDialog {} {
+    global workDir csvParms
+    if {![winfo exists .wiring.csvImport]} {
+        toplevel .wiring.csvImport
+        frame .wiring.csvImport.delimiters
+        label .wiring.csvImport.delimiters.columnarLabel -text "Columnar"
+        ttk::checkbutton .wiring.csvImport.delimiters.columnar -variable csvParms(columnar) -command {
+            csvDialog.spec.columnar $csvParms(columnar)
+            csvDialog.requestRedraw
+        }
+        label .wiring.csvImport.delimiters.separatorLabel -text Separator
+        ttk::combobox .wiring.csvImport.delimiters.separatorValue -values {
+            "," ";" "<tab>" "<space>"} -textvariable csvParms(separator) -width 5
+        bind .wiring.csvImport.delimiters.separatorValue <<ComboboxSelected>> {
+            csvDialog.spec.separator $csvParms(separator)}
+        label .wiring.csvImport.delimiters.decSeparatorLabel -text "Decimal Separator"
+        ttk::combobox .wiring.csvImport.delimiters.decSeparatorValue -values {
+            "." ","} -textvariable csvParms(decSeparator) -width 5
+        bind .wiring.csvImport.delimiters.decSeparatorValue <<ComboboxSelected>> {
+            csvDialog.spec.decSeparator $csvParms(decSeparator)}
+        label .wiring.csvImport.delimiters.escapeLabel -text Escape
+        ttk::combobox .wiring.csvImport.delimiters.escapeValue -values {
+            "\\"} -textvariable csvParms(escape) -width 5
+        bind .wiring.csvImport.delimiters.escapeValue <<ComboboxSelected>> {
+            csvDialog.spec.escape $csvParms(escape)}
+        label .wiring.csvImport.delimiters.quoteLabel -text Quote
+        ttk::combobox .wiring.csvImport.delimiters.quoteValue -values {
+            "'" "\"" } -textvariable csvParms(quote) -width 5
+        bind .wiring.csvImport.delimiters.quoteValue <<ComboboxSelected>> {
+            csvDialog.spec.quote $csvParms(quote)}
+        label .wiring.csvImport.delimiters.mergeLabel -text "Merge Delimiters"
+        ttk::checkbutton .wiring.csvImport.delimiters.mergeValue -variable csvParms(mergeDelimiters) -command {
+            csvDialog.spec.mergeDelimiters $csvParms(mergeDelimiters)
+        }
+        label .wiring.csvImport.delimiters.missingLabel -text "Missing Value"
+        ttk::combobox .wiring.csvImport.delimiters.missingValue \
+            -textvariable csvParms(missingValue) -values {nan 0} -width 5
+        bind .wiring.csvImport.delimiters.missingValue <<ComboboxSelected>> {
+            csvDialog.spec.missingValue $csvParms(missingValue)}
+
+        label .wiring.csvImport.delimiters.colWidthLabel -text "Col Width"
+        spinbox .wiring.csvImport.delimiters.colWidth -from 10 -to 500 -increment 10 \
+            -width 5 -command {adjustColWidth %s} -validate key -validatecommand {adjustColWidth %P}
+        .wiring.csvImport.delimiters.colWidth set 50
+        
+        pack .wiring.csvImport.delimiters.columnarLabel .wiring.csvImport.delimiters.columnar \
+            .wiring.csvImport.delimiters.separatorLabel .wiring.csvImport.delimiters.separatorValue \
+            .wiring.csvImport.delimiters.decSeparatorLabel .wiring.csvImport.delimiters.decSeparatorValue \
+            .wiring.csvImport.delimiters.escapeLabel .wiring.csvImport.delimiters.escapeValue \
+            .wiring.csvImport.delimiters.quoteLabel .wiring.csvImport.delimiters.quoteValue \
+            .wiring.csvImport.delimiters.mergeLabel .wiring.csvImport.delimiters.mergeValue \
+            .wiring.csvImport.delimiters.missingLabel .wiring.csvImport.delimiters.missingValue \
+            .wiring.csvImport.delimiters.colWidthLabel .wiring.csvImport.delimiters.colWidth -side left
+
+        pack .wiring.csvImport.delimiters
+        
+        frame .wiring.csvImport.horizontalName
+        label .wiring.csvImport.horizontalName.text -text "Horizontal dimension"
+        entry .wiring.csvImport.horizontalName.value -textvariable csvParms(horizontalDimension)
+
+        label .wiring.csvImport.horizontalName.duplicateKeyLabel -text "Duplicate Key Action"
+        ttk::combobox .wiring.csvImport.horizontalName.duplicateKeyValue \
+            -textvariable csvParms(duplicateKeyValue) \
+            -values {throwException sum product min max av} -width 15
+        bind .wiring.csvImport.horizontalName.duplicateKeyValue <<ComboboxSelected>> {
+            csvDialog.spec.duplicateKeyAction $csvParms(duplicateKeyValue)}
+
+        pack .wiring.csvImport.horizontalName.duplicateKeyLabel .wiring.csvImport.horizontalName.duplicateKeyValue -side left
+        pack .wiring.csvImport.horizontalName.text .wiring.csvImport.horizontalName.value -side left
+        pack .wiring.csvImport.horizontalName
+
+        image create cairoSurface csvDialogTable -surface csvDialog
+        label .wiring.csvImport.table -image csvDialogTable -width 800 -height 300
+        pack .wiring.csvImport.table -fill both -expand 1 -side top
+
+        scale .wiring.csvImport.hscroll -orient horiz -from -100 -to 1000 -showvalue 0 -command scrollTable
+        pack .wiring.csvImport.hscroll -fill x -expand 1 -side top
+        
+        buttonBar .wiring.csvImport {}
+        # redefine OK command to not delete the the import window on error
+        global csvImportFailed
+        set csvImportFailed 0
+        .wiring.csvImport.buttonBar.ok configure -command csvWebImportDialogOK
+        bind .wiring.csvImport.table <Configure> "csvDialog.requestRedraw"
+        bind .wiring.csvImport.table <Button-1> {csvImportButton1 %x %y};
+        bind .wiring.csvImport.table <ButtonRelease-1> {csvImportButton1Up %x %y %X %Y};
+        bind .wiring.csvImport.table <B1-Motion> {
+            csvDialog.xoffs [expr $csvImportPanX+%x];
+            if $movingHeader {
+                set row [csvDialog.rowOver %y]
+                if {$row>=4} {csvDialog.spec.headerRow [expr $row-4]}
+                csvDialog.flashNameRow [expr $row==3]
+            }
+            csvDialog.requestRedraw
+        }
+    }
+       
+    set urlfile [inputUrl]
+    
+    #set filename [tk_getOpenFile -filetypes {{CSV {.csv}} {All {.*}}} -initialfile [getFile $urlfile] ]  
+    
+    set filename [getFile $urlfile]
+    
+    if [string length $filename] {
+        set workDir [file dirname $filename ]
+        csvDialog.loadFile $filename
+        set csvParms(filename) $filename
+        set csvParms(separator) [csvDialog.spec.separator]
+        set csvParms(decSeparator) [csvDialog.spec.decSeparator]
+        set csvParms(escape) [csvDialog.spec.escape]
+        set csvParms(quote) [csvDialog.spec.quote]
+        set csvParms(mergeDelimiters) [csvDialog.spec.mergeDelimiters]
+        set csvParms(missingValue) [csvDialog.spec.missingValue]
+        set csvParms(duplicateKeyValue) [csvDialog.spec.duplicateKeyAction]
+        set csvParms(horizontalDimension) [csvDialog.spec.horizontalDimName]
+        .wiring.csvImport.delimiters.colWidth set [csvDialog.colWidth]
+        wm deiconify .wiring.csvImport
+        raise .wiring.csvImport
+        csvDialog.requestRedraw
+    }  
 }
 
 proc csvImportDialogOK {} {
@@ -132,6 +256,80 @@ proc csvImportDialogOK {} {
         cancelWin .wiring.csvImport
     }
 }
+
+proc csvWebImportDialogOK {} {
+    global csvParms
+    csvDialog.spec.horizontalDimName $csvParms(horizontalDimension)
+    set filename $csvParms(filename)
+    set csvImportFailed [catch "loadVariableFromCSV csvDialog.spec {$filename}" err]
+    if $csvImportFailed {
+        toplevel .csvImportError
+        label .csvImportError.errMsg -text $err
+        label .csvImportError.msg -text "Would you like to generate a report?"
+        pack .csvImportError.errMsg .csvImportError.msg -side top
+        buttonBar .csvImportError "doReport {$filename}"                
+    } else {
+        reset
+        cancelWin .wiring.csvImport
+    }
+}
+
+# Allow user to input the full web address of a desired file to opened
+proc inputUrl {} {
+	set url "http://www.patreon.com/hpcoder"
+    toplevel .csvImportURL
+    pack [entry .csvImportURL.e -textvariable $url]
+    pack [button .csvImportURL.b -text "OK" -command {destroy .csvImportURL}]
+    bind .csvImportURL <Return> {.csvImportURL.b invoke}
+    focus .csvImportURL.e
+    return $url
+} 
+
+#proc getFile { url } { 
+#   set token [::http::geturl $url]
+#   set data [::http::data $token]
+#   ::http::cleanup $token          
+#   return $data
+#}
+
+# Return contents of a file on the web
+proc getFile { url } {
+  global workDir	
+  set f [open $workDir/download.tar wb]
+  set tok [http::geturl $url -channel $f -binary 1]
+  set data [::http::data $tok]
+  close $f
+  if {[http::status $tok] eq "ok" && [http::ncode $tok] == 200} {
+      puts "Downloaded successfully"
+  }
+  http::cleanup $tok
+  return $data
+}
+
+#proc openCSVFromURL {URL} {
+#    global tcl_platform
+#    if {[tk windowingsystem]=="win32"} {
+#        #shellOpen $URL
+#        set command [list {*}[auto_execok start] {}]
+#        if {[file isdirectory $url]} {
+#            # if there is an executable named eg ${url}.exe, avoid opening that instead:
+#            set url [file nativename [file join $url .]]             
+#    } elseif {$tcl_platform(os)=="Darwin"} {
+#        #exec open $URL
+#        set command [list open]
+#    #} elseif [catch {exec xdg-open $URL &}] {
+#    } elseif [catch {set command [list xdg-open]}] {
+#        # try a few likely suspects
+#        foreach browser {firefox konqueror seamonkey opera} {
+#            set browserNotFound [catch {exec $browser $URL &}]
+#            if {!$browserNotFound} break
+#        }
+#        if $browserNotFound {
+#            tk_messageBox -detail "Unable to find a working web browser, 
+#please consult $URL" -type ok -icon warning
+#        }
+#    }
+#}
 
 proc doReport {inputFname} {
     global workDir
--- gui-tk/wiring.tcl
+++ gui-tk/wiring.tcl
@@ -302,7 +302,7 @@ proc addVariablePostModal {} {
     global globals
     global varInput
     global varType
-
+    
     set name [string trim $varInput(Name)]
     set varExists [variableValues.count $name]
     minsky.addVariable $name $varInput(Type)
@@ -410,9 +410,9 @@ proc textOK {} {
     } elseif [string match "\[%#\]*" $textBuffer] {
         addNote [string range $textBuffer 1 end]
     } else {
-        if [regexp "(.*)=(.*)" $textBuffer dummy name init] {
+        if [regexp "(.*)=(.*)" $textBuffer dummy name init] {            
             minsky.addVariable $name flow
-            minsky.canvas.itemFocus.init $init
+			minsky.canvas.itemFocus.init $init
             minsky.variableValues.reset
         } else {
             minsky.addVariable $textBuffer flow
@@ -659,6 +659,7 @@ proc contextMenu {x y X Y} {
             .wiring.context add command -label "Flip" -command "$item.flip; flip_default"
             if {[$item.type]=="parameter"} {
                 .wiring.context add command -label "Import CSV" -command CSVImportDialog
+                .wiring.context add command -label "Import CSV from web" -command CSVWebImportDialog
             }
             .wiring.context add command -label "Export as CSV" -command exportItemAsCSV
         }
@@ -1179,10 +1180,10 @@ proc editVar {} {
     wm title .wiring.editVar "Edit [$item.name]"
     # populate combobox with existing variable names
     .wiring.editVar.entry10 configure -values [accessibleVars]
+    
 
     set "editVarInput(Name)" [$item.name]
     set "editVarInput(Type)" [$item.type]
-
     set "editVarInput(Initial Value)" [$item.init]
     set "editVarInput(Units)" [value.units.str]
     set "editVarInput(Rotation)" [$item.rotation]
