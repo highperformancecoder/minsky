91aea05aebbf45b9166aaf8f2fbaf554d7568e7f
wdednam <wdednam@gmail.com>
2020-07-13 00:05:03 +0200

    pesky z factor in onResize conditional crept back into Group::clickType

--- model/group.cc
+++ model/group.cc
@@ -758,7 +758,7 @@ namespace minsky
     double w=0.5*iconWidth*z, h=0.5*iconHeight*z;
     // check if (x,y) is within portradius of the 4 corners
     if ((abs(x-left()) < portRadius*z || abs(x-right()) < portRadius*z) &&
-      (abs(y-top()) < portRadius*z || abs(y-bottom()*z) < portRadius*z))
+      (abs(y-top()) < portRadius*z || abs(y-bottom()) < portRadius*z))
       return ClickType::onResize;         
     if (displayContents() && inIORegion(x,y)==IORegion::none)
       return ClickType::outside;
