4d82b0a35200c8cb1a10a3f7096e73da1f1f6f7f
wdednam <wdednam@gmail.com>
2020-08-26 15:38:02 +0200

    implemented correct suggested handling of var names with numerical prefixes which may also signify  multiples of the vars

--- model/godleyIcon.cc
+++ model/godleyIcon.cc
@@ -284,8 +284,11 @@ namespace minsky
                 // set initial value of stock var to init value of flow that is defined by a parameter or a constant. for ticket 1137
                 if (auto initVar=minsky().definingVar(VariableValue::valueId(group.lock(),fc.str())))
                   if (initVar->inputWired() && initVar->type()==VariableType::flow)
-                    if (auto lhsVar=initVar->ports[1]->wires()[0]->from()->item().variableCast())
-                       v.init=lhsVar->vValue()->init;              
+                    if (auto lhsVar=initVar->ports[1]->wires()[0]->from()->item().variableCast()) {
+                      FlowCoef fc1(lhsVar->vValue()->init);
+                      fc1.coef*=fc.coef;
+                      v.init=fc1.str();	
+                    }       
                 v.godleyOverridden=true;
               }
             else
