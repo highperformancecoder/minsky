5c9e833dc087447101559243c8422d108212c01a
wdednam <wdednam@gmail.com>
2020-09-02 21:40:10 +0200

    attempt to improve CSVDialog coverage almost but not quite successful...

---
+++ test/00/importCSV.sh
@@ -0,0 +1,54 @@
+#! /bin/sh
+
+here=`pwd`
+if test $? -ne 0; then exit 2; fi
+tmp=/tmp/$$
+mkdir $tmp
+if test $? -ne 0; then exit 2; fi
+cd $tmp
+if test $? -ne 0; then exit 2; fi
+
+fail()
+{
+    echo "FAILED" 1>&2
+    cd $here
+    chmod -R u+w $tmp
+    rm -rf $tmp
+    exit 1
+}
+
+pass()
+{
+    echo "PASSED" 1>&2
+    cd $here
+    chmod -R u+w $tmp
+    rm -rf $tmp
+    exit 0
+}
+
+trap "fail" 1 2 3 15
+
+cat >input.tcl <<EOF
+source $here/test/assert.tcl
+proc afterMinskyStarted {} {
+  minsky.addVariable par parameter
+  assert {[findObject Variable:parameter]}  
+  editItem
+  assert {[winfo ismapped .wiring.editVar]}     
+  .wiring.editVar.buttonBar.import invoke
+  CSVImportDialog
+  assert {[winfo ismapped .wiring.csvImport]}      
+  minsky.value.csvDialog.url https://sourceforge.net/p/minsky/ravel/20/attachment/BIS_GDP.csv
+  minsky.value.csvDialog.loadFile   
+  # Both this button invoke and csvImportDialogOK fail with segmentation fault here and I don't understand why...
+  #after 2000 {.wiring.csvImport.buttonBar.ok invoke}
+  csvImportDialogOK
+ 
+  after 1000 {tcl_exit}
+}
+EOF
+
+$here/gui-tk/minsky input.tcl
+if [ $? -ne 0 ]; then fail; fi
+
+pass
