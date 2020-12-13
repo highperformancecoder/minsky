4cf10e6f89ca800b20a0be7c174d0e11da46d08d
Russell Standish <hpcoder@hpcoders.com.au>
2020-12-13 14:24:17 +1100

    Split userFunction implementation into two files to get around Windows' object size limitations. For feature #9.

--- model/userFunction.cc
+++ model/userFunction.cc
@@ -17,7 +17,6 @@
   along with Minsky.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "expressionWalker.h"
 #include "userFunction.h"
 #include "evalOp.h"
 #include "selection.h"
@@ -30,7 +29,6 @@ namespace minsky
 
   namespace {
     exprtk::parser<double> parser;
-    exprtk::parser<UnitsExpressionWalker> unitsParser;
   }
   
   template <> void Operation<OperationType::userFunction>::iconDraw(cairo_t*) const
@@ -42,8 +40,6 @@ namespace minsky
     localSymbols.add_variable("y",y);
     compiledExpression.register_symbol_table(externalSymbols);
     compiledExpression.register_symbol_table(localSymbols);
-    parser.enable_unknown_symbol_resolver();
-    unitsParser.enable_unknown_symbol_resolver();
   }
 
   
@@ -52,6 +48,7 @@ namespace minsky
     // do an initial parse to pick up references to external variables
     externalSymbols.clear();
     parser.compile(expression, compiledExpression);
+    parser.disable_unknown_symbol_resolver();
     std::vector<std::string> externalVariables;
     externalSymbols.get_variable_list(externalVariables);
 
@@ -80,49 +77,6 @@ namespace minsky
     return compiledExpression.value();
   }
 
-  Units UserFunction::units(bool check) const
-  {
-    UnitsExpressionWalker x,y;
-    x.units=ports[1]->units(check); x.check=check;
-    y.units=ports[2]->units(check); y.check=check;
-
-    exprtk::symbol_table<UnitsExpressionWalker> symbolTable, unknownVariables;
-    exprtk::expression<UnitsExpressionWalker> compiled;
-    compiled.register_symbol_table(unknownVariables);
-    compiled.register_symbol_table(symbolTable);
-    symbolTable.add_variable("x",x);
-    symbolTable.add_variable("y",y);
-
-    // do an initial parse to pick up references to external variables
-    unitsParser.compile(expression, compiled);
-    std::vector<std::string> externalVariables;
-    unknownVariables.get_variable_list(externalVariables);
-
-    unknownVariables.clear();
-    vector<UnitsExpressionWalker> externalUnits;
-    externalUnits.reserve(externalVariables.size());
-    for (auto& i: externalVariables)
-      {
-        auto v=minsky().variableValues.find(VariableValue::valueIdFromScope(group.lock(),i));
-        if (v!=minsky().variableValues.end())
-          {
-            externalUnits.emplace_back();
-            externalUnits.back().units=v->second->units;
-            externalUnits.back().check=check;
-            unknownVariables.add_variable(i, externalUnits.back());
-          }
-      }
-
-    unitsParser.compile(expression, compiled);
-    try
-      {
-        return compiled.value().units;
-      }
-    catch (const std::exception& ex)
-      {
-        throw_error(ex.what());
-      }
-  }
 
   
 }
