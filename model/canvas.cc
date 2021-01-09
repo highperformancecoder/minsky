ede843d4fb234cc7a104bc6200a7828bd59c956a
wdednam <wdednam@gmail.com>
2021-01-09 13:29:10 +0100

    implemented suggestions on previous PR

--- model/canvas.cc
+++ model/canvas.cc
@@ -697,8 +697,6 @@ namespace minsky
 	           
                 selection.autoLayout();
 	           
-                if (!existingParms.empty()) existingParms.clear();
-	           
                 // Attach mouse focus only to first visible item in selection. For ticket 1098.      
                 for (auto& i: selection.items)
                   if (i->visible())
@@ -708,10 +706,7 @@ namespace minsky
                     }
                                    
                 if (!copyOfGroups.empty()) setItemFocus(copyOfGroups[0]);           
-              } else {
-              p->moveContents(*g);
-              deleteItem();
-            }
+              } else deleteItem();
           }       
         
         // else item is toplevel which can't be ungrouped
