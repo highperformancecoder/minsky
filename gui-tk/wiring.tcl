193d54981cf5cc5ef0293a07da4d20adb5d7055e
Russell Standish <hpcoder@hpcoders.com.au>
2021-10-08 10:22:46 +1100

    Only enable bodgy double-click handling for MacOSX

--- gui-tk/wiring.tcl
+++ gui-tk/wiring.tcl
@@ -234,20 +234,28 @@ proc wrapHoverMouse {op x y} {
     after 3000 hoverMouse
 }
 
-#bind .wiring.canvas <Double-Button-1> {doubleButton %x %y}
-set buttonDownHandler ""
-set buttonUpHandler ""
-set doubleClickTimeout 200
-bind .wiring.canvas <ButtonPress-1> {
-    if [catch {after info $buttonDownHandler}] {
-        set buttonDownHandler [after $doubleClickTimeout wrapHoverMouse mouseDown %x %y]
-    } else {
-        doubleButton %x %y
-        after cancel $buttonDownHandler
-        after cancel $buttonUpHandler
+# On Macs, double-click binding is buggered, so bodgy up a simulated double-click
+# This simulation is not as good as when tk does it properly, however.
+if {[tk windowingsystem] == "aqua"} {
+    set buttonDownHandler ""
+    set buttonUpHandler ""
+    set doubleClickTimeout 200
+    bind .wiring.canvas <ButtonPress-1> {
+        if [catch {after info $buttonDownHandler}] {
+            set buttonDownHandler [after $doubleClickTimeout wrapHoverMouse mouseDown %x %y]
+        } else {
+            doubleButton %x %y
+            after cancel $buttonDownHandler
+            after cancel $buttonUpHandler
+        }
     }
+    bind .wiring.canvas <ButtonRelease-1> {set buttonUpHandler [after $doubleClickTimeout wrapHoverMouse mouseUp %x %y]}
+} else {
+    bind .wiring.canvas <ButtonPress-1> {wrapHoverMouse mouseDown %x %y}
+    bind .wiring.canvas <ButtonRelease-1> {wrapHoverMouse mouseUp %x %y]}
+    bind .wiring.canvas <Double-Button-1> {doubleButton %x %y}
 }
-bind .wiring.canvas <ButtonRelease-1> {set buttonUpHandler [after $doubleClickTimeout wrapHoverMouse mouseUp %x %y]}
+
 bind .wiring.canvas <$meta-ButtonPress-1> {wrapHoverMouse controlMouseDown %x %y}
 bind .wiring.canvas <Motion> {wrapHoverMouse mouseMove %x %y}
 bind .wiring.canvas <Leave> {after cancel hoverMouse}
