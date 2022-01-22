92974006011d6545b476da9e7977ca536800c756
Russell Standish <hpcoder@hpcoders.com.au>
2022-01-22 18:27:39 +1100

    Release 2.22.0-rest.38

--- makeRelease.sh
+++ makeRelease.sh
@@ -1,5 +1,6 @@
 echo "old version=`git describe`"
 echo '#define MINSKY_VERSION "'$1'"' >minskyVersion.h
 sed -i -e "s/\<!--version--\>.*\<\/string\>/\<!--version--\>\<string\>$1\<\/string\>/" minsky.app/Contents/Info.plist
+sed -i -e "s/\"version\":.*,/\"version\":$1,/" gui-js/package.json
 git commit -a -m "Release $1"
 git tag -a -m "" $1
--- minskyVersion.h
+++ minskyVersion.h
@@ -1,1 +1,1 @@
-#define MINSKY_VERSION "2.22.0-rest.37"
+#define MINSKY_VERSION "2.22.0-rest.38"
