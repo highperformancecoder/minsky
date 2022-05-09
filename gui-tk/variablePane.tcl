f13ec821371a671bd75d142274748986ced0d744
Russell Standish <hpcoder@hpcoders.com.au>
2022-05-09 12:24:17 +1000

    VariablePane drawing now.

--- gui-tk/variablePane.tcl
+++ gui-tk/variablePane.tcl
@@ -3,6 +3,7 @@ proc variablePane {} {
         toplevel .variablePane
         image create cairoSurface variableImage -surface minsky.variablePane
         label .variablePane.canvas -image variableImage -width 300 -height 1000
+        pack .variablePane.canvas
     }
     minsky.variablePane.update [.variablePane.canvas cget -width] [.variablePane.canvas cget -height]
     minsky.variablePane.requestRedraw
