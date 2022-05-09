0212e348ecc742940c448d9946cf48bb029c847d
Russell Standish <hpcoder@hpcoders.com.au>
2022-05-09 15:02:25 +1000

    More variable pane tweaks

--- gui-tk/variablePane.tcl
+++ gui-tk/variablePane.tcl
@@ -1,12 +1,13 @@
 proc variablePane {} {
     if {![winfo exists .variablePane]} {
         toplevel .variablePane
+        wm title .variablePane "Variables"
         image create cairoSurface variableImage -surface minsky.variablePane
         label .variablePane.canvas -image variableImage -width 300 -height 1000
-        pack .variablePane.canvas
+        pack .variablePane.canvas -fill both -expand 1
+        bind .variablePane.canvas <Configure> {minsky.variablePane.update %w %h}
     }
     minsky.variablePane.update [.variablePane.canvas cget -width] [.variablePane.canvas cget -height]
-    minsky.variablePane.requestRedraw
     raise .variablePane
 }
 
