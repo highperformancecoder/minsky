954507c8b7c93787ab8ed196793c65cf1bac5968
Russell Standish <hpcoder@hpcoders.com.au>
2022-05-09 18:44:05 +1000

    Variable pane type filtering buttons.

--- gui-tk/variablePane.tcl
+++ gui-tk/variablePane.tcl
@@ -1,7 +1,27 @@
+set checkButtons {}
+set varTypes {parameter flow integral stock}
+
 proc variablePane {} {
+    global varTypes checkButtons
     if {![winfo exists .variablePane]} {
         toplevel .variablePane
         wm title .variablePane "Variables"
+        # selection buttons 
+        frame .variablePane.buttons
+        button .variablePane.buttons.all -text All -command {
+            foreach button $checkButtons {$button select}
+            minsky.variablePane.selection $varTypes
+            minsky.variablePane.update [.variablePane.canvas cget -width] [.variablePane.canvas cget -height]
+        }
+        pack .variablePane.buttons.all -side left
+        foreach type $varTypes {
+            label .variablePane.buttons.${type}Text -text $type
+            checkbutton .variablePane.buttons.$type -variable variablePaneButtons($type) -command "variablePaneButtonProc $type"
+            lappend checkButtons .variablePane.buttons.$type
+            .variablePane.buttons.$type select
+            pack .variablePane.buttons.$type .variablePane.buttons.${type}Text  -side left
+        }
+        pack .variablePane.buttons
         image create cairoSurface variableImage -surface minsky.variablePane
         label .variablePane.canvas -image variableImage -width 300 -height 1000
         pack .variablePane.canvas -fill both -expand 1
@@ -10,9 +30,18 @@ proc variablePane {} {
         bind .variablePane <KeyRelease-Shift_L> {minsky.variablePane.shift 0; .variablePane.canvas configure -cursor {}}
         bind .variablePane.canvas <ButtonPress-1> {catch {minsky.variablePane.mouseDown %x %y}}
         bind .variablePane.canvas <ButtonRelease-1> {minsky.variablePane.mouseUp %x %y}
-        bind .variablePane.canvas <B1-Motion> {minsky.variablePane.mouseMove %x %y; minsky.variablePane.requestRedraw}
+        bind .variablePane.canvas <B1-Motion> {minsky.variablePane.mouseMove %x %y}
     }
     minsky.variablePane.update [.variablePane.canvas cget -width] [.variablePane.canvas cget -height]
     raise .variablePane
 }
 
+proc variablePaneButtonProc {type} {
+    global variablePaneButtons
+    if {$variablePaneButtons($type)} {
+        minsky.variablePane.select $type
+    } else {
+        minsky.variablePane.deselect $type
+    }
+    minsky.variablePane.update [.variablePane.canvas cget -width] [.variablePane.canvas cget -height]
+}
