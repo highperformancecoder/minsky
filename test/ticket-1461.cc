1370c87f6478783042c8ae0d3d0ed74d19f3dd33
Russell Standish <hpcoder@hpcoders.com.au>
2022-07-28 16:51:40 +1000

    Missing file.

---
+++ test/ticket-1461.cc
@@ -0,0 +1,52 @@
+/*
+  @copyright Steve Keen 2022
+  @author Russell Standish
+  This file is part of Minsky.
+
+  Minsky is free software: you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  Minsky is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with Minsky.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "minsky.h"
+#include "minsky_epilogue.h"
+#include <UnitTest++/UnitTest++.h>
+using namespace minsky;
+
+SUITE(Ticket1461)
+{
+  TEST(clone)
+    {
+      Variable<VariableType::flow> var;
+      var.tooltip="hello";
+      auto clone=var.clone();
+      CHECK_EQUAL("hello",clone->tooltip);
+    }
+
+  TEST(copy)
+    {
+      auto& m=minsky::minsky();
+      m.canvas.addVariable("foo", VariableType::flow);
+      auto& var=*m.canvas.itemFocus->variableCast();
+      var.tooltip="hello";
+      var.setUnits("m");
+      m.canvas.selection.insertItem(m.canvas.itemFocus);
+      m.copy();
+      m.paste();
+      auto& varCopy=*m.canvas.itemFocus->variableCast();
+      CHECK(&var!=&varCopy);
+      CHECK_EQUAL("hello",varCopy.tooltip);
+      CHECK_EQUAL("m",varCopy.unitsStr());
+    }
+
+}
+
