b491381c8237bae04f646c7decd6363694d5b4c9
Russell Standish <hpcoder@hpcoders.com.au>
2022-09-23 17:40:54 +1000

    Split RestService instantiation of operators into separate compilation unit.

--- model/operation.cc
+++ model/operation.cc
@@ -17,7 +17,6 @@
   along with Minsky.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "geometry.h"
-#define OPNAMEDEF
 #include "operation.h"
 #include "dataOp.h"
 #include "userFunction.h"
@@ -28,9 +27,6 @@
 
 #include <cairo_base.h>
 #include <pango.h>
-#include "item.rcd"
-#include "operation.rcd"
-#include "operationType.rcd"
 #include "minsky_epilogue.h"
 
 #include <math.h>
@@ -1228,68 +1224,3 @@ namespace minsky
 
 }
 
-#define DEFOP(type) CLASSDESC_ACCESS_EXPLICIT_INSTANTIATION(minsky::Operation<minsky::OperationType::type>);
-
-DEFOP(constant)
-DEFOP(time)
-DEFOP(integrate)
-DEFOP(differentiate)
-DEFOP(data)
-DEFOP(ravel)
-DEFOP(euler)
-DEFOP(pi)
-DEFOP(zero)
-DEFOP(one)
-DEFOP(inf)
-DEFOP(percent)
-DEFOP(add)
-DEFOP(subtract)
-DEFOP(multiply)
-DEFOP(divide)
-DEFOP(min)
-DEFOP(max)
-DEFOP(and_)
-DEFOP(or_)
-DEFOP(log)
-DEFOP(pow)
-DEFOP(polygamma)
-DEFOP(lt)
-DEFOP(le)
-DEFOP(eq)
-DEFOP(userFunction)
-DEFOP(copy)
-DEFOP(sqrt)
-DEFOP(exp)
-DEFOP(ln)
-DEFOP(sin)
-DEFOP(cos)
-DEFOP(tan)
-DEFOP(asin)
-DEFOP(acos)
-DEFOP(atan)
-DEFOP(sinh)
-DEFOP(cosh)
-DEFOP(tanh)
-DEFOP(abs)
-DEFOP(floor)
-DEFOP(frac)
-DEFOP(not_)
-DEFOP(Gamma)
-DEFOP(fact)
-DEFOP(sum)
-DEFOP(product)
-DEFOP(infimum)
-DEFOP(supremum)
-DEFOP(any)
-DEFOP(all)
-DEFOP(infIndex)
-DEFOP(supIndex)
-DEFOP(runningSum)
-DEFOP(runningProduct)
-DEFOP(difference)
-DEFOP(innerProduct)
-DEFOP(outerProduct)
-DEFOP(index)
-DEFOP(gather)
-DEFOP(numOps)
-CLASSDESC_ACCESS_EXPLICIT_INSTANTIATION(minsky::NamedOp);
