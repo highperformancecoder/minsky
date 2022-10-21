eb1feb90acc5f0cab5bd6e25265b4902e1635e82
Russell Standish <hpcoder@hpcoders.com.au>
2022-10-21 18:56:46 +1100

    Plot widget view

--- gui-js/libs/ui-components/src/lib/plot-widget-view/plot-widget-view.component.ts
+++ gui-js/libs/ui-components/src/lib/plot-widget-view/plot-widget-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ElectronService, WindowUtilityService } from '@minsky/core';
-import { commandsMapping } from '@minsky/shared';
+import { PlotWidget } from '@minsky/shared';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe';
 
 @AutoUnsubscribe()
@@ -55,11 +55,8 @@ export class PlotWidgetViewComponent implements OnInit, OnDestroy {
       this.height &&
       this.width
     ) {
-      const command = `${commandsMapping.GET_NAMED_ITEM}/"${this.itemId}"/second/renderFrame [${this.systemWindowId},${this.leftOffset},${this.topOffset},${this.width},${this.height},-1]`;
-
-      this.electronService.sendMinskyCommandAndRender({
-        command,
-      });
+      new PlotWidget(this.electronService.minsky.namedItems.elem(this.itemId.toString()).second)
+        .renderFrame(this.systemWindowId,this.leftOffset,this.topOffset,this.width,this.height,-1);
     }
   }
 
