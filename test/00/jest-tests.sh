b3634e1f4951019deed9c3c92a8b3733448f7cea
Russell Standish <hpcoder@hpcoders.com.au>
2023-03-28 16:46:06 +1100

    Hacked things until the regression tests pass in the CI environment.

--- test/00/jest-tests.sh
+++ test/00/jest-tests.sh
@@ -8,9 +8,11 @@ npm test minsky-web
 #npm test menu
 # why is npm test failing?
 #npm test shared -- --runInBand
-pushd libs/shared
+
 # a bit clunky, but npm test shared doesn't seem to do the right thing.
-ln -sf ../../../examples ..
+mkdir -p libs/examples
+cp ../examples/GoodwinLinear02.mky libs/examples
+pushd libs/shared
 jest
 popd
 #npm test ui-components
