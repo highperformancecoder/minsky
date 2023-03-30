5bc5358e88bfe849cd2640421938ac1e92733fda
Russell Standish <hpcoder@hpcoders.com.au>
2023-03-30 18:47:36 +1100

    popd in wrong place

--- test/00/jest-tests.sh
+++ test/00/jest-tests.sh
@@ -6,16 +6,17 @@ npm test minsky-web
 # commented out tests currently failing
 #npm test core
 #npm test menu
-# why is npm test failing?
-#npm test shared -- --runInBand
 
-# minsky.test.ts crashing on Github action
+# why is npm test failing?
+#    npm test shared -- --skip-nx-cache --runInBand
+
+# shared tests crashing on Github action
 if [ "$TRAVIS" != 1 ]; then
 # a bit clunky, but npm test shared doesn't seem to do the right thing.
     mkdir -p libs/examples
     cp ../examples/GoodwinLinear02.mky libs/examples
     pushd libs/shared
     jest 
+    popd
 fi
-popd
 #npm test ui-components
