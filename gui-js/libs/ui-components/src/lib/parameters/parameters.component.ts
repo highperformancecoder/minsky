a534c6121df9b74b3f481aa24e2d8c4da554ff1f
Russell Standish <hpcoder@hpcoders.com.au>
2023-04-19 18:51:21 +1000

    Convert variables tab to a tree browser version, ordered by variable type.

--- gui-js/libs/ui-components/src/lib/parameters/parameters.component.ts
+++ gui-js/libs/ui-components/src/lib/parameters/parameters.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { ScaleHandler } from '../scale-handler/scale-handler.class';
 import { uniqBy, sortBy } from 'lodash-es';
+import * as JSON5 from 'json5';
 
 @Component({
   selector: 'minsky-parameters',
@@ -16,6 +17,7 @@ export class ParametersComponent {
   tensorText = '<tensor>';
 
   type: string;
+  types: string[];
 
   scale = new ScaleHandler();
 
@@ -31,10 +33,28 @@ export class ParametersComponent {
   }
 
   prepareVariables(variables: any[]) {
+    this.variables={};
+    for (let v in variables) {
+      let type=variables[v].type;
+      if (this.variables[type])
+        this.variables[type].push(variables[v]);
+      else
+        this.variables[type]=[variables[v]];
+    }
+
+    this.types=Object.keys(this.variables);
+    this.types.sort();
+    for (let type in this.variables)
+      this.variables[type].sort((x,y)=>{return x.name<y.name;});
     // why tf is this uniqBy necessary?! sort duplicates rows..? js' own .sort function does the same thing
-    this.variables = uniqBy(sortBy(variables, ['type', 'name']), v => v.name);
+    //  this.variables[type] = uniqBy(sortBy(variables[type], ['name']), v => v.name);
   }
 
+  toggleCaret(event) {
+    event.target.classList.toggle("caret-down");
+    event.target.querySelector(".nested").classList.toggle("active");
+  }
+  
   changeScale(e) {
     if(e.ctrlKey) {
       this.scale.changeScale(e.deltaY);
