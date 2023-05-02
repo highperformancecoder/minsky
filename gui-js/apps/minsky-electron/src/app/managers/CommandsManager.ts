d004bcf0f3cc95726132a2ffcd71848a24b8e768
Russell Standish <hpcoder@hpcoders.com.au>
2023-05-02 19:20:54 +1000

    Increase sizes of EditOperation and EditUserFunction dialogues. For #1571.

--- gui-js/apps/minsky-electron/src/app/managers/CommandsManager.ts
+++ gui-js/apps/minsky-electron/src/app/managers/CommandsManager.ts
@@ -607,12 +607,12 @@ export class CommandsManager {
         height = 240;
         break;
       case ClassType.Operation:
-        height = 180;
-        width = 300;
+        height = 250;
+        width = 350;
         break;
       case ClassType.UserFunction:
-        height = 180;
-        width = 300;
+        height = 250;
+        width = 350;
         break;
       default:
         height = 410;
