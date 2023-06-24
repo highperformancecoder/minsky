5ad12d6fcbe60969490b8fdf82630733f443b210
Russell Standish <hpcoder@hpcoders.com.au>
2023-06-24 17:29:31 +1000

    Remove squirrel events.

--- gui-js/apps/minsky-electron/src/main.ts
+++ gui-js/apps/minsky-electron/src/main.ts
@@ -1,15 +1,10 @@
 import { app, BrowserWindow } from 'electron';
 import App from './app/app';
 import ElectronEvents from './app/events/electron.events';
-import SquirrelEvents from './app/events/squirrel.events';
 import { Utility } from '@minsky/shared';
 
 export default class Main {
   static initialize() {
-    if (SquirrelEvents.handleEvents()) {
-      // squirrel event handled (except first run event) and app will exit in 1000ms, so don't do anything else
-      app.quit();
-    }
   }
 
   static bootstrapApp() {
