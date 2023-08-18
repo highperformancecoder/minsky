ec974d0ff745a4739084fa2a37f65a86637155fe
Russell Standish <hpcoder@hpcoders.com.au>
2023-08-19 02:27:55 +1000

    Set numCols to be the maximum line size of the imported file. For Ravel #113.

--- model/CSVDialog.cc
+++ model/CSVDialog.cc
@@ -432,12 +432,10 @@ std::vector<std::vector<std::string>> CSVDialog::parseLines()
     parsedLines=::parseLines
       (boost::escaped_list_separator<char>(spec.escape,spec.separator,spec.quote),
        initialLines);
-  if (spec.headerRow<parsedLines.size())
-    spec.numCols=parsedLines[spec.headerRow].size();
-  else if (parsedLines.empty())
-    spec.numCols=0;
-  else
-    spec.numCols=parsedLines.front().size();
+
+  spec.numCols=0;
+  for (auto& i: parsedLines)
+    spec.numCols=std::max(spec.numCols, i.size());
   return parsedLines;
 }
 
