69d4847f01d859d0e53b428c6d54d0d5562c4649
Russell Standish <hpcoder@hpcoders.com.au>
2024-07-30 01:27:26 -0700

    Switch to ravel livery in MacOSX builds.

--- gui-js/beforePackHook.js
+++ gui-js/beforePackHook.js
@@ -1,10 +1,18 @@
 let glob=require('glob');
 exports.default = async function(context) {
   let p=context.packager;
-  if (p.platform.name==="windows") {
+  switch (p.platform.name)
+  {
+    case "windows":
     p.appInfo.productName="ravel";
     p.appInfo.sanitizedProductName="ravel";
     p.platformSpecificBuildOptions.icon="apps/minsky-electron/src/assets/RavelLogo.ico"
+    break;
+    case  "darwin":
+    p.appInfo.productName="ravel";
+    p.appInfo.sanitizedProductName="ravel";
+    p.platformSpecificBuildOptions.icon="apps/minsky-electron/src/assets/RavelLogo.icns"
+    break;
   }
   // if libravel is present in dynamic_libraries, this is a ravel build.
   let files=glob.globSync(p.info.projectDir+"/dynamic_libraries/libravel.*")
--- mkMacRESTService.sh
+++ mkMacRESTService.sh
@@ -19,7 +19,7 @@ if [ $version = '"unknown"' ]; then
     version=0.0.0.0
 fi
 
-target=gui-js/dist/executables/minsky-$version.dmg
+target=gui-js/dist/executables/ravel-$version.dmg
 
 # determine release or beta depending on the number of fields separated by '-' in the version string
 numFields=`echo $version|tr - ' '|wc -w`
@@ -68,6 +68,8 @@ rewrite_dylibs $MAC_DIST_DIR/minskyRESTService.node
 pushd gui-js
 npm run export:package:mac
 popd
+
+mv gui-js/dist/executables/minsky-$version.dmg $target
 
 # notarytool is introduced from Big Sur onwards, altool has been deprecated.
 if [ `sw_vers|grep ProductVersion|cut -f2|cut -f1 -d.` -lt 11 ]; then
