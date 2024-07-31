41fd3a16a2b18a7767b3eda3c0721a324a792187
Russell Standish <hpcoder@hpcoders.com.au>
2024-07-31 15:20:07 +1000

    executable name reset to Minsky for Linux builds.

--- gui-js/beforePackHook.js
+++ gui-js/beforePackHook.js
@@ -1,20 +1,6 @@
 let glob=require('glob');
 exports.default = async function(context) {
   let p=context.packager;
-  switch (p.platform.name)
-  {
-    case "windows":
-    p.appInfo.productName="ravel";
-    p.appInfo.sanitizedProductName="ravel";
-    p.platformSpecificBuildOptions.icon="apps/minsky-electron/src/assets/RavelLogo.ico"
-    break;
-    case  "darwin":
-    p.appInfo.name="ravel";
-    p.appInfo.productName="ravel";
-    p.appInfo.sanitizedProductName="ravel";
-    p.platformSpecificBuildOptions.icon="apps/minsky-electron/src/assets/RavelLogo.icns"
-    break;
-  }
   // if libravel is present in dynamic_libraries, this is a ravel build.
   let files=glob.globSync(p.info.projectDir+"/dynamic_libraries/libravel.*")
   if (files.length>0) {
