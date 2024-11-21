addfd82a7f5e2276c416e8b2bf324dba46ce9b1d
Russell Standish <hpcoder@hpcoders.com.au>
2024-11-21 16:48:35 +1100

    Add Fedora 40 to obsCheck

--- obsCheck/smokeTest.sh
+++ obsCheck/smokeTest.sh
@@ -11,7 +11,7 @@ for i in Dockerfile-*[^~]; do
     case $i in
         Dockerfile-debian) versions="11 12";;
         Dockerfile-ubuntu) versions="22.04 23.04 23.10 24.04 24.10";;
-        Dockerfile-fedora) versions="37 38 39";;
+        Dockerfile-fedora) versions="37 38 39 40";;
         Dockerfile-leap) versions="15.4 15.5";;
         *) versions=default;;
     esac
