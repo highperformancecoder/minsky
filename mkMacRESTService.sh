3099cb0f00af1725ab05362130bc7488e14e1bbf
Russell Standish <hpcoder@hpcoders.com.au>
2025-04-02 17:58:11 +1100

    Added libsoci dylib to Minsky bundle on MacOSX.

--- mkMacRESTService.sh
+++ mkMacRESTService.sh
@@ -78,6 +78,8 @@ mkdir -p $MAC_DIST_DIR
 
 rewrite_dylibs $MAC_DIST_DIR/minskyRESTService.node
 rewrite_dylibs $MAC_DIST_DIR/pyminsky.so
+cp /opt/local//lib/libsoci_core.4.0.dylib $MAC_DIST_DIR/
+rewrite_dylibs $MAC_DIST_DIR/libsoci_core.4.0.dylib
 
 pushd gui-js
 # libravel.so expects certain dylibs in node-addons for legacy
