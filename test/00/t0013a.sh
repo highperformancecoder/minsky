213cf0561d2a96fa2350c55ea11ff0a4cd45c885
Russell Standish <hpcoder@hpcoders.com.au>
2025-04-15 10:41:58 +1000

    Pythonsied t0013a

--- test/00/t0013a.sh
+++ test/00/t0013a.sh
@@ -1,66 +1,42 @@
 #! /bin/sh
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
-
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
-
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
-
-trap "fail" 1 2 3 15
-
+. $here/test/common-test.sh
 # double entry bookkeeping tests
 
 # insert ecolab script code here
 # use \$ in place of $ to refer to variable contents
 # exit 0 to indicate pass, and exit 1 to indicate failure
-cat >input.tcl <<EOF
-source $here/test/assert.tcl
-minsky.addGodley
-set item minsky.canvas.itemFocus
-minsky.getItemAt [\$item.x] [\$item.y]
-\$item.table.clear
-\$item.table.resize 4 4
-\$item.table.setDEmode 1
-\$item.setCell 0 1 s1
-\$item.setCell 0 2 s2
-\$item.setCell 0 3 s3
-\$item.setCell 1 0 "Initial Conditions"
-\$item.setCell 1 1 10
-\$item.setCell 1 3 10
-\$item.setCell 2 1 a
-\$item.setCell 2 2 b
-\$item.setCell 2 3 a
+cat >input.py <<EOF
+import sys
+sys.path.insert(0, '$here')
+from pyminsky import minsky
 
-\$item.table.assetClass 1 asset
-\$item.table.assetClass 2 liability
-\$item.table.assetClass 3 liability
-assert {[minsky.canvas.item.table.rowSum 1]==0} ""
-assert {[minsky.canvas.item.table.rowSum 2]=="-b"} ""
-assert {"[minsky.canvas.item.table.getVariables]"=="a b"} ""
-tcl_exit
+minsky.canvas.addGodley()
+item=minsky.canvas.itemFocus
+minsky.canvas.getItemAt(item.x(), item.y())
+item.table.clear()
+item.table.resize(4, 4)
+item.table.setDEmode(True)
+item.setCell(0, 1, 's1')
+item.setCell(0, 2, 's2')
+item.setCell(0, 3, 's3')
+item.setCell(1, 0, "Initial Conditions")
+item.setCell(1, 1, '10')
+item.setCell(1, 3, '10')
+item.setCell(2, 1, 'a')
+item.setCell(2, 2, 'b')
+item.setCell(2, 3, 'a')
+
+item.table._assetClass(1, 'asset')
+item.table._assetClass(2, 'liability')
+item.table._assetClass(3, 'liability')
+assert minsky.canvas.item.table.rowSum(1)=='0'
+assert minsky.canvas.item.table.rowSum(2)=="-b"
+assert minsky.canvas.item.table.getVariables()()==['a', 'b']
 EOF
 
-$here/gui-tk/minsky input.tcl
+python3 input.py
 if test $? -ne 0; then fail; fi
 
 pass
