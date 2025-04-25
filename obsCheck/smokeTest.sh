7eca612f75eecba32279010612f8707b1595736f
Russell Standish <hpcoder@hpcoders.com.au>
2025-04-25 18:44:11 +1000

    Added Ubuntu 25.04 and Fedora 42 to OBS check.

--- obsCheck/smokeTest.sh
+++ obsCheck/smokeTest.sh
@@ -10,8 +10,8 @@ rm *.log
 for i in Dockerfile-*[^~]; do
     case $i in
         Dockerfile-debian) versions="11 12";;
-        Dockerfile-ubuntu) versions="22.04 24.04 24.10";;
-        Dockerfile-fedora) versions="40 41";;
+        Dockerfile-ubuntu) versions="22.04 24.04 24.10 25.04";;
+        Dockerfile-fedora) versions="40 41 42";;
         Dockerfile-leap) versions="15.4 15.5 15.6";;
         *) versions=default;;
     esac
