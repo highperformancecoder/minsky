daf906f721538df526d013d5ea7e2a1f9a7733f3
Russell Standish <hpcoder@hpcoders.com.au>
2025-05-06 15:19:13 +1000

    Pythonise latexTestEq

--- test/00/latexTestEq.sh
+++ test/00/latexTestEq.sh
@@ -1,35 +1,19 @@
 #! /bin/sh
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
+. $here/test/common-test.sh
 
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
+cat >input.py <<EOF
+import sys
+sys.path.insert(0,'$here')
+from pyminsky import minsky
 
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
-
-trap "fail" 1 2 3 15
-
+minsky.load('testEq.mky')
+minsky.latex('testEq.tex', False)
+minsky.latex('testEqWrapped.tex', True)
+EOF
 cp $here/test/testEq.mky .
-$here/gui-tk/minsky $here/test/testEq.tcl
+python3 input.py
 if test $? -ne 0; then fail; fi
 
 
--- test/testEq.tcl
+++
@@ -1,5 +0,0 @@
-#!../minsky
-minsky.load testEq.mky
-minsky.latex testEq.tex 0
-minsky.latex testEqWrapped.tex 1
-tcl_exit
