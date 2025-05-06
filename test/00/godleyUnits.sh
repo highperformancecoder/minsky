efada71937b6ac11dd7b051169cdef2a213afa31
Russell Standish <hpcoder@hpcoders.com.au>
2025-05-06 14:40:01 +1000

    godleyUnits pythonised

--- test/00/godleyUnits.sh
+++ test/00/godleyUnits.sh
@@ -1,60 +1,47 @@
 #! /bin/sh
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
+. $here/test/common-test.sh
 
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
+cat >input.py <<EOF
+import sys
+sys.path.insert(0,'$here')
+from pyminsky import minsky, findObject, findVariable
 
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
+minsky.timeUnit('s')
+minsky.canvas.addGodley()
+item=findObject('GodleyIcon')
+item.table.resize(4, 4)
+item.setCell(0, 1, 'a')
+item.setCell(2, 1, 'b')
+item.setCell(3, 1, 'c')
+item.update()
+findVariable('b').setUnits('b')
+findVariable('c').setUnits('c')
 
-trap "fail" 1 2 3 15
-cat >input.tcl <<EOF
-source $here/test/assert.tcl
-minsky.timeUnit s
-minsky.addGodley
-minsky.findObject GodleyIcon
-minsky.canvas.item.table.resize 4 4
-minsky.canvas.item.setCell 0 1 a
-minsky.canvas.item.setCell 2 1 b
-minsky.canvas.item.setCell 3 1 c
-minsky.canvas.item.update
-minsky.findVariable b
-minsky.canvas.item.setUnits b
-minsky.findVariable c
-minsky.canvas.item.setUnits c
 # should throw incompatible units
-assert {[catch minsky.dimensionalAnalysis]}
-minsky.findVariable c
-minsky.canvas.item.setUnits b
-assert {[catch minsky.dimensionalAnalysis]}
-minsky.findVariable a
-minsky.canvas.item.setUnits "b s"
-assert {[minsky.canvas.item.unitsStr]=="b s"}
-minsky.dimensionalAnalysis
+thrown=False
+try:
+    minsky.dimensionalAnalysis()
+except:
+    thrown=True
+assert thrown
+findVariable('c').setUnits('b')
 
-tcl_exit
+thrown=False
+try:
+    minsky.dimensionalAnalysis()
+except:
+    thrown=True
+assert thrown
+
+item=findVariable('a')
+item.setUnits("b s")
+assert item.unitsStr()=="b s"
+minsky.dimensionalAnalysis()
 EOF
 
-$here/gui-tk/minsky input.tcl
+python3 input.py
 if [ $? -ne 0 ]; then fail; fi
 
 pass
