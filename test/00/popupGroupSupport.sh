fe4b6e30a091dbdf85d15d543904c7128a4334ac
Russell Standish <hpcoder@hpcoders.com.au>
2025-05-07 17:05:57 +1000

    Various regression tests aren't including common-test.

--- test/00/popupGroupSupport.sh
+++ test/00/popupGroupSupport.sh
@@ -1,32 +1,8 @@
 #! /bin/sh
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
+. $here/test/common-test.sh
 
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
-
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
-
-trap "fail" 1 2 3 15
 cat >input.py <<EOF
 from pyminsky import minsky
 
--- test/00/simpleIntegrationTest.sh
+++ test/00/simpleIntegrationTest.sh
@@ -3,32 +3,7 @@
 # test that a simple integral example works when both coupled and uncoupled
 
 here=`pwd`
-if test $? -ne 0; then exit 2; fi
-tmp=/tmp/$$
-mkdir $tmp
-if test $? -ne 0; then exit 2; fi
-cd $tmp
-if test $? -ne 0; then exit 2; fi
-
-fail()
-{
-    echo "FAILED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 1
-}
-
-pass()
-{
-    echo "PASSED" 1>&2
-    cd $here
-    chmod -R u+w $tmp
-    rm -rf $tmp
-    exit 0
-}
-
-trap "fail" 1 2 3 15
+. $here/test/common-test.sh
 
 cat >input.py <<EOF
 from pyminsky import minsky, findObject
