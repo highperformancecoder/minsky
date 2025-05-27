762fceb5ebbd6cf83640b198e133a513ae193393
Russell Standish <hpcoder@hpcoders.com.au>
2025-05-27 15:45:54 +1000

    Add version number to doxygen generated documentation. For Ravel #686.

--- makeRelease.sh
+++ makeRelease.sh
@@ -2,7 +2,8 @@ echo "old version=`git describe`"
 echo '#define MINSKY_VERSION "'$1'"' >minskyVersion.h
 echo 'export const version="'$1'";' >gui-js/libs/shared/src/lib/constants/version.ts
 echo "\\author{Version $1}" >doc/version.tex
-sed -i -e "s/\<!--version--\>.*\<\/string\>/\<!--version--\>\<string\>$1\<\/string\>/" minsky.app/Contents/Info.plist
+echo "PROJECT_NAME=Minsky:&nbsp;$1" >Doxyversion
+psed -i -e "s/\<!--version--\>.*\<\/string\>/\<!--version--\>\<string\>$1\<\/string\>/" minsky.app/Contents/Info.plist
 sed -i -e "s/\"version\":.*,/\"version\":\"$1\",/" gui-js/package.json
 sed -i -e "1,10s/\"version\":.*,/\"version\": \"$1\",/" gui-js/package-lock.json
 git commit -a -S -m "Release $1"
