b23c2770a52e6db91abd375e0848b8452912e658
Russell Standish <hpcoder@hpcoders.com.au>
2025-05-28 11:49:00 +1000

    Remove extraneous character from makeRelease  script

--- makeRelease.sh
+++ makeRelease.sh
@@ -3,7 +3,7 @@ echo '#define MINSKY_VERSION "'$1'"' >minskyVersion.h
 echo 'export const version="'$1'";' >gui-js/libs/shared/src/lib/constants/version.ts
 echo "\\author{Version $1}" >doc/version.tex
 echo "PROJECT_NAME=Minsky:&nbsp;$1" >Doxyversion
-psed -i -e "s/\<!--version--\>.*\<\/string\>/\<!--version--\>\<string\>$1\<\/string\>/" minsky.app/Contents/Info.plist
+sed -i -e "s/\<!--version--\>.*\<\/string\>/\<!--version--\>\<string\>$1\<\/string\>/" minsky.app/Contents/Info.plist
 sed -i -e "s/\"version\":.*,/\"version\":\"$1\",/" gui-js/package.json
 sed -i -e "1,10s/\"version\":.*,/\"version\": \"$1\",/" gui-js/package-lock.json
 git commit -a -S -m "Release $1"
