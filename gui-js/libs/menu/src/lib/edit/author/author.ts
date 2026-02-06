732a3a998acffae78178b01e073e06dfc0dfdc70
Russell Standish <hpcoder@hpcoders.com.au>
2026-02-06 17:00:55 +1100

    Added missing files.

---
+++ gui-js/libs/menu/src/lib/edit/author/author.ts
@@ -0,0 +1,42 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ElectronService, WindowUtilityService } from '@minsky/core';
+import { GodleyIcon} from '@minsky/shared';
+import { Subject, takeUntil } from 'rxjs';
+import { InputModalComponent} from '@minsky/ui-components';
+
+
+@Component({
+    selector: 'author',
+    templateUrl: './author.html',
+    styleUrls: ['./author.scss'],
+    standalone: true,
+    imports: [InputModalComponent],
+})
+export class AuthorComponent implements OnDestroy {
+  author: string;
+
+  destroy$ = new Subject<{}>();
+  
+  constructor(
+    private route: ActivatedRoute,
+    private electronService: ElectronService,
+    private windowUtilityService: WindowUtilityService
+  ) {
+    this.route.queryParams.pipe(takeUntil(this.destroy$)).subscribe((params) => {
+      this.author=params.author;
+    });
+  }
+
+  async handleEditAuthor(author: string) {
+    if (this.electronService.isElectron) {
+      this.electronService.minsky.author(author);
+    }
+    this.windowUtilityService.closeCurrentWindowIfNotMain();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(undefined);
+    this.destroy$.complete();
+  }
+}
