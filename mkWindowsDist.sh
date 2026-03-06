afda7aef0215888e0728da32ac15efdecc88e9b5
Russell Standish <hpcoder@hpcoders.com.au>
2026-03-06 15:28:43 +1100

    Deep code signing of the Windows build.

--- mkWindowsDist.sh
+++ mkWindowsDist.sh
@@ -1,11 +1,24 @@
-# Place the path to your certificate store in environment variable
-# WINDOWS_SIGN_CERTIFICATE_NAME and the password to open it at
-# WINDOWS_SIGN_TOKEN_PASSWORD
-
 version=`cut -f3 -d' ' minskyVersion.h|head -1|tr -d '"'`
 pushd gui-js
 export NX_DAEMON=false # prevent hang in next step
-npm run export:package:windows
+
+# don't use the all in one packager script, as code signing is stuffed
+#npm run export:package:windows
+npm run build:web
+npm run build:electron
+
+rm -rf dist/executables
+
+# 1) build win-unpacked only
+npx electron-builder --win dir --x64 --config.directories.output=dist/executables
+
+# 2) sign payload that will be embedded in installer
+find dist/executables/win-unpacked -type f \( -iname "*.exe" -o -iname "*.node" \) -exec ../sign.sh {} \;
+
+# 3) build NSIS installer from signed prepackaged directory
+npx electron-builder --win nsis --x64 \
+  --prepackaged dist/executables/win-unpacked \
+  --config.directories.output=dist/executables
 
 if [ -f dynamic_libraries/libravel.dll ]; then
     name=ravel-with-plugin
@@ -14,8 +27,8 @@ if [ -f dynamic_libraries/libravel.dll ]; then
 else
     name=ravel
 fi
-#java -jar $HOME/usr/bin/jsign-4.1.jar --keystore $WINDOWS_SIGN_CERTIFICATE_NAME --storetype PKCS12 --storepass "$WINDOWS_SIGN_TOKEN_PASSWORD" dist/executables/$name-$version.exe
-sudo java -jar $HOME/usr/bin/jsign-6.0.jar --storetype ETOKEN --keystore ~/usr/etc/SafeNet.cfg -t http://timestamp.sectigo.com --storepass "$WINDOWS_SIGN_TOKEN_PASSWORD" dist/executables/$name-$version.exe
+
+../sign.sh dist/executables/$name-$version.exe
 echo cp dist/executables/$name-$version.exe /tmp
 cp dist/executables/$name-$version.exe /tmp
 popd
