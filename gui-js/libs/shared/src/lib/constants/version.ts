a7d1401dbfe2f51a8601cd26a274f87205a8b379
Russell Standish <hpcoder@hpcoders.com.au>
2026-06-08 09:24:49 +1000

    Release 3.29.0-beta.1

--- gui-js/libs/shared/src/lib/constants/version.ts
+++ gui-js/libs/shared/src/lib/constants/version.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const version="3.28.0-beta.1";
-=======
-export const version="3.28.0";
->>>>>>> master
+export const version="3.29.0-beta.1";
--- minskyVersion.h
+++ minskyVersion.h
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-#define MINSKY_VERSION "3.28.0-beta.1"
-=======
-#define MINSKY_VERSION "3.28.0"
->>>>>>> master
+#define MINSKY_VERSION "3.29.0-beta.1"
