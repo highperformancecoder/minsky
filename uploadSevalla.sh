c75f78c44c86d72934392db02fbe1ad9e6904b27
Russell Standish <hpcoder@hpcoders.com.au>
2026-06-08 12:07:09 +1000

    chore: Translate x86_64 to x64 in uploadSevalla script. For #1938.

--- uploadSevalla.sh
+++ uploadSevalla.sh
@@ -4,7 +4,7 @@ apiUrl=https://minskybe-x7dj1.sevalla.app
 version=`git describe`
 releaseDate=`git log -1 --format=%cs --date=iso`
 arch=`arch`
-if [ $arch = 'i386' ]; then arch=x64; fi
+if [ $arch = 'i386' -o $arch = 'x86_64' ]; then arch=x64; fi
 os=`uname`
 
 if git describe|grep beta; then
